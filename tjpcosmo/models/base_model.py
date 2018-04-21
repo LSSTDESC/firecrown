decfd18841fb282cc823a748546d112d7ca3cde8
Joe Zuntz <joezuntz@googlemail.com>
2018-04-21 17:37:27 +0100

    add missing files

---
+++ tjpcosmo/models/base_model.py
@@ -0,0 +1,57 @@
+model_registry = {}
+
+
+class AnalysisModel:
+    theory_calculator_class = None
+    theory_results_class = None
+    data_class = None
+    metadata_class = None
+
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        name = cls.name if hasattr(cls, 'name') else cls.__name__
+        name = name.lower()
+        print(f"Register {name}")
+        model_registry[name] = cls
+
+
+    """
+    This is the superclass for all Models, which represent the part of the
+    likelihood function that generate mean theory predictions from parameters.
+
+    This is just the skeleton - there are various more complete designs in the sandbox.
+    Delete this notice after implementing them!
+
+    """
+
+    def __init__(self, config, data_info, likelihood_class):
+        """
+        Instantiate the model from a dictionary of options.
+
+        Subclasses usually override this to do their own instantiation.
+        They should call this parent method first.
+        """
+        self.config=config
+        self.data = self.data_class.load(data_info)
+        self.metadata = self.extract_metadata(data_info)
+        self.theory_calculator = self.theory_calculator_class(config, self.metadata)
+        self.likelihood = likelihood_class(self.data)
+
+    @staticmethod
+    def from_name(name):
+        return model_registry[name]
+
+    def extract_metadata(self, data_info):
+        pass
+
+    def run(self, parameterSet):
+        theory_results = self.theory_calculator.run(parameterSet)
+        like = self.likelihood.run(theory_results)
+        return like, theory_results
+
+    def likelihood(self, parameterSet):
+        return self.run[0]
+
+
+
