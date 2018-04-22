ae6b81b1d110ab755855d25e2a5de25ca0b6641b
Joe Zuntz <joezuntz@googlemail.com>
2018-04-22 22:12:33 +0100

    move calculator methods into super class

--- tjpcosmo/analyses/twopoint/calculator.py
+++ tjpcosmo/analyses/twopoint/calculator.py
@@ -1,8 +1,6 @@
 import pyccl as ccl
 from ..base_calculator import TheoryCalculator
 from .theory_results import TwoPointTheoryResults
-from ...systematics import Systematic, SourceSystematic, OutputSystematic, CosmologySystematic
-from ..sources import make_source, LSSSource
 import numpy as np
 def make_fake_source(name, stype, metadata):
     import numpy as np
@@ -63,72 +61,7 @@ def convert_cosmobase_to_ccl(cosmo_base):
 class TwoPointTheoryCalculator(TheoryCalculator):
     def __init__(self, config, metadata):
         super().__init__(config, metadata)
-        print("Warning! Making a fake source")
-        self.setup_systematics(config['systematics'])
-        self.setup_sources(config)
-        self.metadata = metadata
 
-    def setup_systematics(self, sys_config):
-        self.systematics = {}
-        for name, info in sys_config.items():
-            sys = Systematic.from_info(name, info)
-            self.systematics[name] = sys
-        
-        self.output_systematics = {
-            name:sys
-            for name,sys in self.systematics.items()
-            if isinstance(sys,OutputSystematic)
-        }
-
-        self.cosmology_systematics = {
-            name:sys
-            for name,sys in self.systematics.items()
-            if isinstance(sys,CosmologySystematic)
-        }
-
-        self.source_systematics = {
-            name:sys
-            for name,sys in self.systematics.items()
-            if isinstance(sys,SourceSystematic)
-        }
-
-
-    def setup_sources(self, config):
-        info = config['sources']
-        used_systematics = set()
-        self.sources = []
-
-        for name, source_info in info.items():
-            stype = source_info['type']
-
-            source = make_source(name, stype, self.metadata)
-
-            sys_names = source_info.get('systematics', [])
-            # check if string or list
-            for sys_name in sys_names:
-                sys = self.systematics.get(sys_name)
-                if sys is None:
-                    raise ValueError(f"Systematic with name {sys_name} was specified for source {name} but not defined in parameter file systematics section")
-                source.systematics.append(sys)
-                used_systematics.add(sys_name)
-            self.sources.append(source)
-
-        for sys_name in self.source_systematics.keys():
-            if sys_name not in used_systematics:
-                raise ValueError(f"Systematic with name {sys_name} was specified in param file but never used")
-
-    def update_systematics(self, parameters):
-        for sys in self.systematics.values():
-            sys.update(parameters)
-
-    def apply_source_systematics(self, cosmo):
-        sources = {}
-        for source in self.sources:
-            for syst in source.systematics:
-                if isinstance(syst, SourceSystematic):
-                    source = syst(cosmo, source)
-            sources[source.name] = source
-        return sources            
 
     def apply_output_systematics(self, c_ell_pair):
         pass
