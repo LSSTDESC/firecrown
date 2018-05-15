3afef09381193fc9fad42d1596b7d59e9bffce8a
Shahab Joudaki <joudaki@asosxlap150.nat.physics.ox.ac.uk>
2018-05-15 17:13:15 +0100

    updated tdist

--- tjpcosmo/likelihood/tdist.py
+++ tjpcosmo/likelihood/tdist.py
@@ -2,6 +2,11 @@ import numpy as np
 from .base import BaseLikelihood
 
 class Tdist(BaseLikelihood):
+    def __init__(self, data):
+        BaseLikelihood.__init__(self, data)
+        if self.data.nsims <= 0 :
+            raise ValueError('nsims needs to be a positive integer read in from SACC.')
+
     def run(self, theory_results):
         """T-distribution for the likelihood in the case of numerical covariance (Sellentin & Heavens 2016; arXiv:1511.05969). 
         As the number of simulations increases, the t-distribution approaches a Gaussian. Hartlap factor no longer needed.
@@ -11,8 +16,6 @@ class Tdist(BaseLikelihood):
         mu = self.data.data_vector
         P = self.data.precision
         nsims = self.data.nsims
-        if nsims <= 0 :
-            raise ValueError('nsims needs to be a positive integer read in from SACC.')
         delta = d-mu
         chi2 = float(np.einsum('i,ij,j', delta, P, delta))
         like = -0.5 * nsims*np.log(1.0 + chi2/(nsims-1.0))
