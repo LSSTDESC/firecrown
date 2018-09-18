df2b2d7ee31d7dec57fb486cfb44ef5eb5389781
Matthew R Becker <becker.mr@gmail.com>
2018-09-18 18:53:18 -0500

    linting

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 scripts = ['bin/tjpcosmo']
 
-if __name__ == "__main__":
-    setup(name = 'tjpcosmo',
-          description       = "DESC Cosmology Constraints Tool",
-          author            = "DESC Team",
-          packages = find_packages(),
-          include_package_data = True,
-          scripts = scripts,
-          install_requires = ['cosmosis-standalone', 'pyccl'],
-          )
+setup(
+    name='tjpcosmo',
+    description="DESC Cosmology Constraints Tool",
+    author="DESC Team",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=scripts,
+    install_requires=['cosmosis-standalone', 'pyccl'],
+)
