ca37027c4213636fd2b5186227ac325fcd898bd0
Matthew R Becker <becker.mr@gmail.com>
2018-10-04 19:32:45 -0500

    ENH update the run function

--- firecrown/run.py
+++ firecrown/run.py
@@ -1,4 +1,3 @@
-from .likelihoods import compute_two_point
 
 
 def compute_loglike(*, cosmo, data):
@@ -9,7 +8,7 @@ def compute_loglike(*, cosmo, data):
     cosmo : a `pyccl.Cosmology` object
         A cosmology.
     data : dict
-        The result of calling `nightvision.config.parse` on an input YAML
+        The result of calling `firecrown.config.parse` on an input YAML
         config.
 
     Returns
@@ -19,31 +18,13 @@ def compute_loglike(*, cosmo, data):
     statistics : dict
         A dictionary of output statistics from each analysis.
     """
-    # first build the sources
-    srcs = {}
-    for src, keys in data['sources'].items():
-        srcs[src] = keys['build_func'](
-            cosmo=cosmo,
-            params=data['parameters'],
-            src_name=src,
-            **keys)
-
-    # now accumulate stats and loglike
     loglike = 0.0
     statistics = {}
     analyses = list(
         set(list(data.keys())) -
-        set(['sources', 'parameters', 'run_metadata']))
+        set(['parameters', 'run_metadata']))
     for analysis in analyses:
-        if analysis == 'two_point':
-            _ll, _stats = compute_two_point(
-                cosmo=cosmo,
-                parameters=data['parameters'],
-                sources=srcs,
-                **data[analysis])
-        else:
-            raise ValueError("Analysis '%s' not recognized!" % analysis)
-
+        _ll, _stats = data[analysis]['eval'](cosmo, data[analysis]['data'])
         loglike += _ll
         statistics[analysis] = _stats
 
