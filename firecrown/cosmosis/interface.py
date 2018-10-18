a3b8e8c32a1b811a9ddcaa4f4ef73430bf62c419
Joe Zuntz <joezuntz@googlemail.com>
2018-10-18 14:08:00 +0100

    make quiet default in cosmosis interface

--- firecrown/cosmosis/interface.py
+++ firecrown/cosmosis/interface.py
@@ -40,7 +40,7 @@ def execute(block, data):
     block['likelihoods', 'firecrown_like'] = loglike
 
     # Unless in quiet mode, print out what we have done
-    if not data['cosmosis']['quiet']:
+    if not data['cosmosis'].get(quiet, True):
         print("params = {}".format(data['parameters']))
         print(f"loglike = {loglike}\n", flush=True)
 
