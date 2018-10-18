1bdc6b608f88f885c83708528fce29875734e844
Matthew R Becker <becker.mr@gmail.com>
2018-10-18 15:05:58 -0500

    ENH added version

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import os
 from setuptools import setup, find_packages
 
 scripts = ['bin/firecrown']
 
+__version__ = None
+pth = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "firecrown",
+    "_version.py")
+with open(pth, 'r') as fp:
+    exec(fp.read())
+
 setup(
     name='firecrown',
+    version=__version__,
     description="DESC Cosmology Constraints Tool",
     author="DESC Team",
     packages=find_packages(),
