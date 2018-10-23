65ef35043d0cfcda23ea64a65e4839fd27c56ada
Matthew R Becker <becker.mr@gmail.com>
2018-10-23 16:21:53 -0700

    add linear alignment sys

--- firecrown/ccl/systematics/wl.py
+++ firecrown/ccl/systematics/wl.py
@@ -1,3 +1,5 @@
+import pyccl as ccl
+
 from ..core import Systematic
 
 __all__ = ['MultiplicativeShearBias']
@@ -36,33 +38,50 @@ class MultiplicativeShearBias(Systematic):
         source.scale_ *= (1.0 + params[self.m])
 
 
-# class LinearAlignment(SourceSystematic):
-#     modified_source_properties = ['ia_amplitude', 'f_red']
-#     required_source_properties = ['z']
-#     params = ['biasia']
-#     optional_params = {
-#         'alphaz': 0.0,
-#         'z_piv': 0.0,
-#         'fred': 1.0,
-#         'alphag': 0.0
-#     }
-#
-#     def adjust_source(self, cosmo, source):
-#         if self.adjust_requirements(source):
-#             pref = 1.0
-#             if self.values['alphaz']:
-#                 pref *= (
-#                     ((1.0 + source.z) / (1.0 + self.values['z_piv'])) **
-#                     self.values['alphaz'])
-#             if self.values['alphag']:
-#                 pref *= ccl.growth_factor(
-#                     cosmo, 1.0 / (1.0 + source.z)) ** self.values['alphag']
-#
-#             source.ia_amplitude[:] = pref*self.values['biasia']
-#             source.f_red[:] = self.values['fred']
-#             return 0
-#         else:
-#             print(
-#                 f"{self.__class__.__name__} did not find all "
-#                 "required source parameters")
-#             return 1
+class LinearAlignmentSystematic(Systematic):
+    """Linear alignment systematic.
+
+    This systematic adds
+
+    Parameters
+    ----------
+    bias_ia : str
+        The name of the intrinsic alignment parameter.
+    alphaz : str
+        The mame of redshift dependence parameter of the intrinsic alignment
+        signal.
+    alphag : str
+        The name of the growth dependence parameter of the intrinsic alignment
+        signal.
+    z_piv : str
+        The name of the pivot redshift parameter for the intrinsic alignment
+        parameter.
+
+    Methods
+    -------
+    apply : appaly the systematic to a source
+    """
+    def __init__(self, bias_ia, alphaz, alphag, z_piv):
+        self.bias_ia = bias_ia
+        self.alphaz = alphaz
+        self.alphag = alphag
+        self.z_piv = z_piv
+
+    def apply(self, cosmo, params, source):
+        """Apply a linear alignment systematic.
+
+        Parameters
+        ----------
+        cosmo : pyccl.Cosmology
+            A pyccl.Cosmology object.
+        params : dict
+            A dictionary mapping parameter names to their current values.
+        source : a source object
+            The source to which apply the shear bias.
+        """
+        pref = (
+            ((1.0 + source.z_) / (1.0 + params[self.z_piv])) **
+            params[self.alphaz])
+        pref *= ccl.growth_factor(
+                cosmo, 1.0 / (1.0 + source.z_)) ** params[self.alphag]
+        source.bias_ia_ *= pref
