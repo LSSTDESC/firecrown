6473527e154345ae13e3a553053073e49f4ce2d1
Matthew R Becker <becker.mr@gmail.com>
2018-10-23 19:03:39 -0700

    added an LSS source

--- firecrown/ccl/sources/sources.py
+++ firecrown/ccl/sources/sources.py
@@ -7,7 +7,7 @@ import pyccl as ccl
 from ..core import Source
 
 
-__all__ = ['WLSource']
+__all__ = ['WLSource', 'NumberCountsSource']
 
 
 class WLSource(Source):
@@ -114,4 +114,115 @@ class WLSource(Source):
                 cosmo,
                 has_intrinsic_alignment=False,
                 n=(self.z_, self.nz_))
+        self.tracer_ = tracer
+
+
+class NumberCountsSource(Source):
+    """A CCL number counts source.
+
+    Parameters
+    ----------
+    nz_data : str
+        The path to the photo-z distribution in a CSV. The columns should be
+        {'z', 'nz'}.
+    bias : str
+        The parameter for the bias of the source.
+    has_rsd : bool, optional
+        If `True`, the source has RSD terms.
+    has_magnification : bool, optional
+        If `True`, the source has magnification terms.
+    mag_bias : str, optional
+        The parameter for the magnification bias of the source. Only used if
+        `has_magnification` is `True`.
+    scale : float, optional
+        The default scale for this source. Usually the default of 1.0 is
+        correct.
+    systematics : list of str, optional
+        A list of the source-level systematics to apply to the source. The
+        default of `None` implies no systematics.
+
+    Attributes
+    ----------
+    nz_interp : Akima1DInterpolator
+        A spline interpolation of the initial photo-z distribution.
+    z_ : np.ndarray, shape (n_z,)
+        The array of redshifts for the photo-z distribution. Set after a call
+        to `render`.
+    nz_ : np.ndarray, shape (n_z,)
+        The photo-z distribution amplitudes.  Set after a call to `render`.
+    bias_ : np.ndarray, shape (n_z,)
+        The bias of the source. Set after a call to `render`.
+    mag_bias_ : np.ndarray, shape (n_z,)
+        The magnification bias of the source. Only used if `has_magnification`
+        is `True` and only set after a call to `render`.
+    scale_ : float
+        The overall scale associated with the source. Set after a call to
+        `render`.
+    tracer_ : `pyccl.CLTracerLensing`
+        The CCL tracer associated with this source. Set after a call to
+        `render`.
+
+    Methods
+    -------
+    render : apply systematics to this source and build the
+        `pyccl.ClTracerNumberCounts`
+    """
+    def __init__(
+            self, nz_data, bias, has_rsd=False, has_magnification=False,
+            mag_bias=None, scale=1.0, systematics=None):
+        self.nz_data = nz_data
+        self.bias = bias
+        self.has_rsd = has_rsd
+        self.has_magnification = has_magnification
+        self.mag_bias = mag_bias
+        self.systematics = systematics or []
+        df = pd.read_csv(nz_data)
+        _z, _nz = df['z'].values.copy(), df['nz'].values.copy()
+        self._z = _z
+        self._nz = _nz
+        self.nz_interp = Akima1DInterpolator(self._z, self._nz)
+        self.scale = scale
+
+    def render(self, cosmo, params, systematics=None):
+        """
+        Render a source by applying systematics.
+
+        Parameters
+        ----------
+        cosmo : pyccl.Cosmology
+            A pyccl.Cosmology object.
+        params : dict
+            A dictionary mapping parameter names to their current values.
+        systematics : dict
+            A dictionary mapping systematic names to their objects. The
+            default of `None` corresponds to no systematics.
+        """
+        systematics = systematics or {}
+
+        self.z_ = self._z.copy()
+        self.nz_ = self._nz.copy()
+        self.scale_ = self.scale
+        self.bias_ = np.ones_like(self.z_) * params[self.bias]
+
+        if self.has_magnification:
+            self.mag_bias_ = np.ones_like(self.z_) * params[self.mag_bias]
+
+        for systematic in self.systematics:
+            systematics[systematic].apply(cosmo, params, self)
+
+        if self.has_magnification:
+            tracer = ccl.ClTracerNumberCounts(
+                cosmo,
+                has_rsd=self.has_rsd,
+                has_magnification=True,
+                n=(self.z_, self.nz_),
+                bias=(self.z_, self.bias_),
+                mag_bias=(self.z_, self.mag_bias_))
+        else:
+            tracer = ccl.ClTracerNumberCounts(
+                cosmo,
+                has_rsd=self.has_rsd,
+                has_magnification=False,
+                n=(self.z_, self.nz_),
+                bias=(self.z_, self.bias_))
         self.tracer_ = tracer
