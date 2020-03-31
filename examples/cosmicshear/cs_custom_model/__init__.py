05fd4e9a7f9b2b17d21ce8caa6ee5cb0e5c89abb
Matthew R. Becker <becker.mr@gmail.com>
2020-03-31 10:29:05 -0500

    more answers

---
+++ examples/cosmicshear/cs_custom_model/__init__.py
@@ -0,0 +1,1 @@
+from .wl import CustomShearBias  # noqa
---
+++ examples/cosmicshear/cs_custom_model/wl.py
@@ -0,0 +1,52 @@
+"""This is a template for you to implement
+
+We are going to build a simple systematic which models the multiplicative
+bias of each tomo bin as
+
+    m_i = m_delta_i + m_base
+
+where m_base is shared between the bins. This model is dumb, but YOLO.
+"""
+from firecrown.ccl.core import Systematic
+
+
+class CustomShearBias(Systematic):
+    """Multiplicative shear bias systematic.
+
+    This systematic adjusts the `scale_` of a source by `(1 + m_delta + m_base)`.
+
+    Parameters
+    ----------
+    m_delta : str
+        The name of the multiplicative bias delta parameter.
+    m_base : str
+        The name of the base multiplicative bias parameter.
+
+    Methods
+    -------
+    apply : appaly the systematic to a source
+    """
+    def __init__(self, m_delta, m_base):
+        # don't do any work in this function
+        # you need to attach the params to the object
+        self.m_delta = m_delta
+        self.m_base = m_base
+
+    def apply(self, cosmo, params, source):
+        """Apply multiplicative shear bias to a source. The `scale_` of the
+        source is multiplied by `(1 + m)`.
+
+        Parameters
+        ----------
+        cosmo : pyccl.Cosmology
+            A pyccl.Cosmology object.
+        params : dict
+            A dictionary mapping parameter names to their current values.
+        source : a source object
+            The source to which apply the shear bias.
+        """
+        # each parameter to a systematic is a string that references a
+        # parameter in the params dict
+
+        # you need to adjust the scale of the input source
+        source.scale_ *= (1.0 + params[self.m_delta] + params[self.m_base])
