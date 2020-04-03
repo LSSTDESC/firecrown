732094c12aa262732f01eab373e86c1ff4221777
Matthew R. Becker <becker.mr@gmail.com>
2020-04-03 12:04:55 -0500

    try again

--- docs/conf.py
+++ docs/conf.py
@@ -24,24 +24,22 @@ if os.path.isdir(root_path):
     sys.path.insert(0, root_path)
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-# if on_rtd:
-#     try:
-#         from unittest.mock import MagicMock
-#     except ImportError:
-#         from mock import Mock as MagicMock
-#
-#         class Mock(MagicMock):
-#             @classmethod
-#             def __getattr__(cls,name):
-#                 return MagicMock()
-#
-#     MOCK_MODULES = [
-#         "firecrown.analysis",
-#         "firecrown.ccl",
-#         "firecrown.cosmosis",
-#     ]
-#
-#     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+if on_rtd:
+    try:
+        from unittest.mock import MagicMock
+    except ImportError:
+        from mock import Mock as MagicMock
+
+        class Mock(MagicMock):
+            @classmethod
+            def __getattr__(cls, name):
+                return MagicMock()
+
+    MOCK_MODULES = [
+        "firecrown.cosmosis.run.cosmosis",
+    ]
+
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- Project information -----------------------------------------------------
