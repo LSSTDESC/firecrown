5a68a4d9c688ef8ef8c4077ca6f8cb0c476acd34
Matthew R. Becker <becker.mr@gmail.com>
2020-04-03 09:31:11 -0500

    TST fixed buggy test

--- firecrown/tests/test_metadata.py
+++ firecrown/tests/test_metadata.py
@@ -8,18 +8,18 @@ from ..metadata import write_metadata
 
 def test_write_metadata(tmpdir):
     analysis_id = uuid.uuid4().hex
+    odir = os.path.join(tmpdir, 'output_%s' % analysis_id)
 
     config_file = os.path.join(tmpdir, 'cnf.yaml')
     with open(config_file, 'w') as fp:
         fp.write('data: 1\n')
 
-    write_metadata(analysis_id, tmpdir, config_file)
+    write_metadata(analysis_id, odir, config_file)
 
     # we are going light on the tests here
     # let's make sure
     # 1) the output files exist
     # 2) they have some correct data (not checking the whole thing, blah)
-    odir = os.path.join(tmpdir, 'output_%s' % analysis_id)
     cfg = os.path.join(odir, 'config.yaml')
     mtd = os.path.join(odir, 'metadata.yaml')
     assert os.path.exists(odir)
