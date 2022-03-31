3f367d8c9677069035238ed2862e745a1aa3b887
Marc Paterno <paterno@fnal.gov>
2022-03-31 17:36:03 -0500

    Update internal version number to 1.0.0

--- firecrown/version.py
+++ firecrown/version.py
@@ -4,8 +4,8 @@ version 2.
 See https://semver.org for details.
 """
 
-firecrown_major = 0
-firecrown_minor = 9
-firecrown_patch = 999
+firecrown_major = 1
+firecrown_minor = 0
+firecrown_patch = 0
 __version__ = f"{firecrown_major}.{firecrown_minor}.{firecrown_patch}"
 
