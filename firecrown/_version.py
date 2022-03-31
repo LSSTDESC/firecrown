d0111c3dbeb81855fdba320354f45aac0cd8fdab
Marc Paterno <paterno@fnal.gov>
2022-03-31 10:01:28 -0500

    New versioning policy

--- firecrown/_version.py
+++ firecrown/_version.py
@@ -1,1 +1,11 @@
-__version__ = "0.3.0"
+"""The versioning of Firecrown is intended to follow *Semantic Vrersioning*,
+version 2.
+
+See https://semver.org for details.
+"""
+
+firecrown_major = 0
+firecrown_minor = 9
+firecrown_patch = 999
+__version__ = f"{firecrown_major}.{firecrown_minor}.{firecrown_patch}"
+
