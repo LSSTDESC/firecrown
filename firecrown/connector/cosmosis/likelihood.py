0b538d145047568c7da88e09746fcaaf2511322e
Marc Paterno <paterno@fnal.gov>
2022-06-21 16:08:23 -0500

    Move method calculate_ccl_args

--- firecrown/connector/cosmosis/likelihood.py
+++ firecrown/connector/cosmosis/likelihood.py
@@ -11,11 +11,10 @@ from typing import Dict
 import cosmosis.datablock
 from cosmosis.datablock import option_section
 from cosmosis.datablock import names as section_names
-import numpy as np
 import pyccl as ccl
 
 from firecrown.connector.mapping import mapping_builder, MappingCosmoSIS
-from firecrown.likelihood.likelihood import load_likelihood
+from firecrown.likelihood.likelihood import load_likelihood, Likelihood
 from firecrown.parameters import ParamsMap
 
 
@@ -35,6 +34,7 @@ class FirecrownLikelihood:
     theory.
     """
 
+    likelihood: Likelihood
     map: MappingCosmoSIS
 
     def __init__(self, config: cosmosis.datablock):
@@ -50,7 +50,7 @@ class FirecrownLikelihood:
         cosmological_params = extract_section(sample, "cosmological_parameters")
         self.map.set_params_from_cosmosis(cosmological_params)
 
-        ccl_args = self.calculate_ccl_args(sample)
+        ccl_args = self.map.calculate_ccl_args(sample)
 
         cosmo = ccl.CosmologyCalculator(**self.map.asdict(), **ccl_args)
 
@@ -76,63 +76,6 @@ class FirecrownLikelihood:
                 sec_dict = extract_section(sample, section)
                 firecrown_params = ParamsMap({**firecrown_params, **sec_dict})
         return firecrown_params
-
-    def calculate_ccl_args(self, sample):
-        """Calculate the arguments necessary for CCL for this sample."""
-        ccl_args = {}
-        if sample.has_section("matter_power_lin"):
-            k = self.map.transform_k_h_to_k(sample["matter_power_lin", "k_h"])
-            z_mpl = sample["matter_power_lin", "z"]
-            scale_mpl = self.map.redshift_to_scale_factor(z_mpl)
-            p_k = self.map.transform_p_k_h3_to_p_k(sample["matter_power_lin", "p_k"])
-            p_k = self.map.redshift_to_scale_factor_p_k(p_k)
-
-            ccl_args["pk_linear"] = {
-                "a": scale_mpl,
-                "k": k,
-                "delta_matter:delta_matter": p_k,
-            }
-            ccl_args["nonlinear_model"] = "halofit"
-
-        # TODO: We should have several configurable modes for this module.
-        # In all cases, an exception will be raised (causing a program
-        # shutdown) if something that is to be read from the DataBlock is not
-        # present in the DataBlock.
-        #
-        # background: read only background information from the DataBlock; it
-        # will generate a runtime error if the configured likelihood attempts
-        # to use anything else.
-        #
-        # linear: read also the linear power spectrum from the DataBlock. Any
-        # non-linear power spectrum present will be ignored. It will generate
-        # a runtime error if the configured likelihood attempts to make use
-        # of a non-linear spectrum.
-        #
-        #  nonlinear: read also the nonlinear power spectrum from the DataBlock.
-        #
-        # halofit, halomodel, emu: use CCL to calculate the nonlinear power
-        # spectrum according to the named technique. In all cases, the linear
-        # power spectrum read from the DataBlock is used as input. In all
-        # cases, it is an error if the DataBlock also contains a nonlinear
-        # power spectrum.
-
-        chi = np.flip(sample["distances", "d_m"])
-        scale_distances = self.map.redshift_to_scale_factor(sample["distances", "z"])
-        # h0 = sample["cosmological_parameters", "h0"]
-        # NOTE: The first value of the h_over_h0 array is non-zero because of the way
-        # CAMB does it calculation. We do not modify this, because we want consistency.
-
-        # hubble_radius_today = (ccl.physical_constants.CLIGHT * 1e-5) / h0
-        # h_over_h0 = np.flip(sample["distances", "h"]) * hubble_radius_today
-        h_over_h0 = self.map.transform_h_to_h_over_h0(sample["distances", "h"])
-
-        ccl_args["background"] = {
-            "a": scale_distances,
-            "chi": chi,
-            "h_over_h0": h_over_h0,
-        }
-
-        return ccl_args
 
 
 def setup(config: cosmosis.datablock) -> FirecrownLikelihood:
