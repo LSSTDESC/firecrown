1f09c853dc1263066af08fdb448f4bf900f85a56
Tilman Troester <tilman.troester@gmail.com>
2022-10-24 22:31:25 +0200

    Change logic of require_nonlinear_pk (#197)
    Co-authored-by: Sandro Dias Pinto Vitenti <vitenti@uel.br>
    Co-authored-by: Marc Paterno <paterno@fnal.gov>

--- firecrown/connector/mapping.py
+++ firecrown/connector/mapping.py
@@ -252,21 +252,20 @@ class MappingCosmoSIS(Mapping):
                 "delta_matter:delta_matter": p_k,
             }
 
-        if self.require_nonlinear_pk:
-            if sample.has_section("matter_power_nl"):
-                k = self.transform_k_h_to_k(sample["matter_power_nl", "k_h"])
-                z_mpl = sample["matter_power_nl", "z"]
-                scale_mpl = self.redshift_to_scale_factor(z_mpl)
-                p_k = self.transform_p_k_h3_to_p_k(sample["matter_power_nl", "p_k"])
-                p_k = self.redshift_to_scale_factor_p_k(p_k)
+        if sample.has_section("matter_power_nl"):
+            k = self.transform_k_h_to_k(sample["matter_power_nl", "k_h"])
+            z_mpl = sample["matter_power_nl", "z"]
+            scale_mpl = self.redshift_to_scale_factor(z_mpl)
+            p_k = self.transform_p_k_h3_to_p_k(sample["matter_power_nl", "p_k"])
+            p_k = self.redshift_to_scale_factor_p_k(p_k)
 
-                ccl_args["pk_nonlin"] = {
-                    "a": scale_mpl,
-                    "k": k,
-                    "delta_matter:delta_matter": p_k,
-                }
-            else:
-                ccl_args["nonlinear_model"] = "halofit"
+            ccl_args["pk_nonlin"] = {
+                "a": scale_mpl,
+                "k": k,
+                "delta_matter:delta_matter": p_k,
+            }
+        elif self.require_nonlinear_pk:
+            ccl_args["nonlinear_model"] = "halofit"
         else:
             ccl_args["nonlinear_model"] = None
 
