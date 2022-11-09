cf1b353ed2cb21a1158f4fe7803add36701ac611
root <eduardojsbarroso@gmail.com>
2022-11-09 16:17:09 -0300

    Implemented Naive Compute method For fake data

--- examples/number_counts/example_numbercount_stat.py
+++ examples/number_counts/example_numbercount_stat.py
@@ -11,7 +11,8 @@ from firecrown.likelihood.gauss_family.statistic.two_point import TwoPoint
 from firecrown.likelihood.gauss_family.gaussian import ConstGaussian
 sacc.__version__
 
-
+cosmo = ccl.Cosmology(Omega_c=0.26, Omega_b=0.04,
+                      h=0.7, sigma8=0.8, n_s=0.96, Neff=3.04,  m_nu=1.0e-05, m_nu_type="single")
 sac_file = sacc.Sacc.load_fits("./clusters.sacc")
 sac_file2 = sacc.Sacc.load_fits("/home/eduardo/firecrown/examples/des_y1_3x2pt/des_y1_3x2pt_sacc_data.fits")
 #print(sac_file.data)
@@ -21,6 +22,8 @@ rp = stats.required_parameters()
 lista_t = [stats]
 lk = ConstGaussian(lista_t)
 lk.read(sac_file)
-
+lk.reset()
+lk.get_derived_parameters()
+print(lk.compute_loglike(cosmo))
 #print(sacc.standard_types)
 #print(sac_file.data)
--- firecrown/likelihood/gauss_family/statistic/number_counts_stat.py
+++ firecrown/likelihood/gauss_family/statistic/number_counts_stat.py
@@ -10,7 +10,7 @@ import warnings
 import numpy as np
 import scipy.interpolate
 
-import pyccl
+import pyccl 
 
 from .statistic import Statistic
 from .source.source import Systematic
@@ -28,11 +28,13 @@ SACC_DATA_TYPE_TO_CCL_KIND = {
 class NumberCountStatsArgs:
     """Class for number counts tracer builder argument."""
 
-    def __init__(self, scale=None, tracers=None, z_min=None, z_max=None, nz=None, metadata=None):
+    def __init__(self, scale=None, tracers=None, z_min=None, z_max=None, logm_min=None, logm_max=None, nz=None, metadata=None):
 
         self.scale = scale
         self.z_min =  z_min
         self.z_max =  z_max
+        self.logm_min =  logm_min
+        self.logm_max =  logm_max
         self.nz = nz
         self.metadata = metadata
 
@@ -98,6 +100,35 @@ class NumberCountStat(Statistic):
 
         return derived_parameters
 
+    def _dmdz_dV (self, logm, z, cosmo):
+        """
+        parameters
+        __________
+        logm: float
+            Cluster mass
+        z : float
+            Cluster Redshift
+        cosmo : pyccl Cosmology
+        
+        reuturn
+        _______
+        
+        integrand : float
+                    Number Counts pdf at z and logm.
+        """
+        a = 1./(1. + z)
+        hmd_200c = pyccl.halos.MassDef(200, 'critical')
+        mass = 10**(logm)
+        hmf_200c = pyccl.halos.MassFuncTinker08(cosmo, mass_def=hmd_200c)
+        nm = hmf_200c.get_mass_function(cosmo, mass, a)
+        da = pyccl.background.angular_diameter_distance(cosmo, a)
+        E = pyccl.background.h_over_h0(cosmo, a)
+        dV = ((1.+z)**2)*(da**2)*pyccl.physical_constants.CLIGHT_HMPC/cosmo['h']/E
+        integrand = nm * dV
+        
+        return integrand
+    
+        
     def read(self, sacc_data):
         """Read the data for this statistic from the SACC file.
 
@@ -108,33 +139,52 @@ class NumberCountStat(Statistic):
         """
 
         tracer = []
-        tracer2 = []
         z_min = []
         z_max = []
+        logm_min = []
+        logm_max = []
         nz = []
+        data = []
         metadata = []
         inds = []
         for i in range (0, len(sacc_data.data)):
             #IM assuming that there is only one tracer. We have to change if there is more. Also Im assuming the entire file is Number Counts
             tracer.append(sacc_data.data[i].tracers[0])
             metadata.append(sacc_data.tracers[tracer[i]].metadata)
+            data.append(sacc_data.data[i].value)
             if sacc_data.data[i].data_type == 'cluster_mass_count_wl':
                 z_min.append(metadata[i]["z_min"])
                 z_max.append(metadata[i]["z_max"])
+                logm_min.append(np.log10(metadata[i]["Mproxy_min"]))
+                logm_max.append(np.log10(metadata[i]["Mproxy_max"]))
                 nz.append(sacc_data.data[i].value)
-       
         self.tracer_args = NumberCountStatsArgs(
-            scale=self.scale, tracers=tracer, z_min=z_min, z_max=z_max, nz=nz, metadata=metadata)
-
+            scale=self.scale, tracers=tracer, z_min=z_min, z_max=z_max, 
+             logm_min=logm_min, logm_max=logm_max, nz=nz, metadata=metadata)
         self.sacc_tracer = tracer
-        
         self.sacc_inds = np.atleast_1d(
-            sacc_data.indices("cluster_mass_count_wl")
+        sacc_data.indices("cluster_mass_count_wl")
         )
+        self.data_vector = data
 
 
     def compute(self, cosmo: pyccl.Cosmology) -> Tuple[np.ndarray, np.ndarray]:
         """Compute a Number Count statistic."""
-
+        z_bins = self.tracer_args.z_min
+        logm_bins = self.tracer_args.logm_min
+        z_bins.append(self.tracer_args.z_max[-1])
+        logm_bins.append(self.tracer_args.logm_max[-1])
+        z_bins = list(set(z_bins))
+        logm_bins = list(set(logm_bins))
+        theory_vector = []
+        def integrand(logm, z):
+            return self._dmdz_dV(logm, z, cosmo)
+        norm = norm = 1./scipy.integrate.dblquad(integrand, z_bins[0], z_bins[-1],
+        lambda x:logm_bins[0], lambda x:logm_bins[-1], epsrel=1.e-4)[0] 
+        for i in range(len(z_bins) -1):
+            for j in range(len(logm_bins)-1):
+                bin_count = scipy.integrate.dblquad(integrand, z_bins[i], z_bins[i+1],
+                lambda x:logm_bins[j], lambda x:logm_bins[j+1], epsrel=1.e-4)[0] 
+                theory_vector.append(bin_count / norm)
 
         return np.array(self.data_vector), np.array(theory_vector)
