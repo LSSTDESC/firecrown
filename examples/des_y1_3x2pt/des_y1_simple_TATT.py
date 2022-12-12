caching PT calculated pks and tracers for more efficient computation; bug fix with multiplying scales from linear and PT contributions; bug fix removing mag_bias from NumberCountsPTArgs; ability to add whether to calculate halo_model pks in TwoPoint function;

examples/des_y1_3x2pt/des_y1_simple_TATT.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import time
 
 if sys.version_info[0] >= 3:
     unicode = str
@@ -39,12 +40,13 @@ ia_systematic = wl.TATTSystematic()
 for i in range(n_source):
     # Define the photo-z shift systematic.
     pzshift = wl.PhotoZShift(sacc_tracer=f"src{i}")
+    mult_bias = wl.MultiplicativeShearBias(sacc_tracer=f"src{i}")
 
     # Create the weak lensing source, specifying the name of the tracer in the
     # sacc file and a list of systematics
     sources[f"src{i}"] = wl.WeakLensingPT(
         sacc_tracer=f"src{i}",
-        systematics=[ia_systematic, pzshift]
+        systematics=[ia_systematic, pzshift], scale=2.0
     )
 
 for i in range(n_lens):
@@ -54,7 +56,7 @@ for i in range(n_lens):
     nl_bias = nc.NLBiasSystematic(sacc_tracer=f"lens{i}")
     sources[f"lens{i}"] = nc.NumberCountsPT(
         sacc_tracer=f"lens{i}",
-        systematics=[pzshift, nl_bias], has_mag_bias=False
+        systematics=[pzshift, nl_bias], has_mag_bias=False, scale=3.0
     )
 
 # Define the statistics we like to include in the likelihood
@@ -70,7 +72,7 @@ for stat, sacc_stat in [
             stats[f"{stat}_src{i}_src{j}"] = TwoPoint(
                 source0=sources[f"src{i}"],
                 source1=sources[f"src{j}"],
-                sacc_data_type=sacc_stat,pt_calc=PTC
+                sacc_data_type=sacc_stat, pt_calc = PTC
             )
 for j in range(n_source):
     for i in range(n_lens):
@@ -86,6 +88,7 @@ for i in range(n_lens):
         source1=sources[f"lens{i}"],
         sacc_data_type="galaxy_density_xi", pt_calc=PTC
     )
+
 # Create the likelihood from the statistics
 lk = ConstGaussian(statistics=list(stats.values()))
 
@@ -102,6 +105,7 @@ if __name__ == "__main__":
     import numpy as np
     import pyccl as ccl
     import matplotlib.pyplot as plt
+    import pyccl.nl_pt as pt
 
     # Define a ccl.Cosmology object using default parameters
     ccl_cosmo = ccl.CosmologyVanillaLCDM()
@@ -115,6 +119,7 @@ if __name__ == "__main__":
                                     "ia_bias_2": 0.5,
                                     "alphaz_2":0.0, 
                                     "alphag_2":0.0,
+                                    "src0_mult_bias": 0.0,
                                     
                                     "lens0_mag_bias":0.0,
                                     "lens0_bias":2.0,
@@ -133,124 +138,112 @@ if __name__ == "__main__":
     # Apply the systematics parameters
     likelihood.update(systematics_params)
     # Compute the log-likelihood, using the ccl.Cosmology object as the input
-    log_like = likelihood.compute_loglike(ccl_cosmo)
+    for i in range(2):
+        start = time.time()
+        log_like = likelihood.compute_loglike(ccl_cosmo)
+        end = time.time()
+        print(f"Log-like = {log_like:.1f}")
+        print(f"time to compute = {end-start: .3f}")
 
-    print(f"Log-like = {log_like:.1f}")
+        # Plot the predicted and measured statistic
+        x = likelihood.statistics[0].ell_or_theta_
+        y_data = likelihood.statistics[0].measured_statistic_
+        y_err = np.sqrt(np.diag(likelihood.cov))[:len(x)]
+        y_theory = likelihood.statistics[0].predicted_statistic_
 
-    # Plot the predicted and measured statistic
-    x = likelihood.statistics[0].ell_or_theta_
-    y_data = likelihood.statistics[0].measured_statistic_
-    y_err = np.sqrt(np.diag(likelihood.cov))[:len(x)]
-    y_theory = likelihood.statistics[0].predicted_statistic_
+        start = time.time()
 
+        src0_tracer = sacc_data.get_tracer("src0")
+        lens0_tracer = sacc_data.get_tracer("lens0")
+        z, nz = src0_tracer.z, src0_tracer.nz
+        lens_z, lens_nz = lens0_tracer.z, lens0_tracer.nz
 
+        # Define a ccl.Cosmology object using default parameters
+        #ccl_cosmo = ccl.CosmologyVanillaLCDM()
+        #ccl_cosmo.compute_nonlin_power()
 
+        # Bare CCL setup
+        a_1 = 1.
+        a_2 = 0.5
+        a_d = 0.5
 
-    import numpy as np
-    import pyccl as ccl
-    import pyccl.nl_pt as pt
-    import matplotlib.pyplot as plt
+        b_1 = 2.0
+        b_2 = 1.0
+        b_s = 1.0
 
-    src0_tracer = sacc_data.get_tracer("src0")
-    lens0_tracer = sacc_data.get_tracer("lens0")
-    z, nz = src0_tracer.z, src0_tracer.nz
-    lens_z, lens_nz = lens0_tracer.z, lens0_tracer.nz
+        mag_bias = 0.0
 
-    # Define a ccl.Cosmology object using default parameters
-    ccl_cosmo = ccl.CosmologyVanillaLCDM()
-    #ccl_cosmo.compute_nonlin_power()
+        c_1, c_d, c_2 = pt.translate_IA_norm(ccl_cosmo, z, a1=a_1, a1delta=a_d, a2=a_2, Om_m2_for_c2=False)
 
-    # Bare CCL setup
-    a_1 = 1.
-    a_2 = 0.5
-    a_d = 0.5
+        #print(c_d)
 
-    b_1 = 2.0
-    b_2 = 1.0
-    b_s = 1.0
+        # Code that creates a Pk2D object:
+        ptc = pt.PTCalculator(with_NC=True, with_IA=True,
+                              log10k_min=-4, log10k_max=2, nk_per_decade=20)
+        ptt_i = pt.PTIntrinsicAlignmentTracer(c1=(z, c_1), c2=(z, c_2), cdelta=(z, c_d))
+        ptt_m = pt.PTMatterTracer()
+        ptt_g = pt.PTNumberCountsTracer(b1=b_1, b2=b_2, bs=b_s)
+        # IA
+        
+        pk_im = pt.get_pt_pk2d(ccl_cosmo, ptt_i, tracer2=ptt_m, ptc=ptc)
+        pk_ii = pt.get_pt_pk2d(ccl_cosmo, ptt_i, ptc=ptc)
+        pk_gi = pt.get_pt_pk2d(ccl_cosmo, ptt_g, tracer2=ptt_i, ptc=ptc)
+        # Galaxies
+        pk_gm = pt.get_pt_pk2d(ccl_cosmo, ptt_g, tracer2=ptt_m, ptc=ptc)
+        pk_gg = pt.get_pt_pk2d(ccl_cosmo, ptt_g, ptc=ptc)
+        # Magnification
 
-    mag_bias = 0.0
+        # Plot the predicted and measured statistic
+        x = likelihood.statistics[0].ell_or_theta_
+        x_minus = likelihood.statistics[1].ell_or_theta_
+        x_ggl = likelihood.statistics[2].ell_or_theta_
+        x_nc = likelihood.statistics[3].ell_or_theta_
+        y_data = likelihood.statistics[0].measured_statistic_
+        y_err = np.sqrt(np.diag(likelihood.cov))[:len(x)]
+        y_theory = likelihood.statistics[0].predicted_statistic_
+        y_theory_minus = likelihood.statistics[1].predicted_statistic_
+        y_theory_ggl = likelihood.statistics[2].predicted_statistic_
+        y_theory_nc = likelihood.statistics[3].predicted_statistic_
 
-    c_1, c_d, c_2 = pt.translate_IA_norm(ccl_cosmo, z, a1=a_1, a1delta=a_d, a2=a_2, Om_m2_for_c2=False)
+        #print(list(likelihood.statistics[0].cells.keys()))
 
-    #print(c_d)
+        ells = likelihood.statistics[0].ells
+        ells_minus = likelihood.statistics[1].ells
+        ells_ggl = likelihood.statistics[2].ells
+        ells_nc = likelihood.statistics[3].ells
 
-    # Code that creates a Pk2D object:
-    ptc = pt.PTCalculator(with_NC=True, with_IA=True,
-                          log10k_min=-4, log10k_max=2, nk_per_decade=20)
-    ptt_i = pt.PTIntrinsicAlignmentTracer(c1=(z, c_1), c2=(z, c_2), cdelta=(z, c_d))
-    ptt_m = pt.PTMatterTracer()
-    ptt_g = pt.PTNumberCountsTracer(b1=b_1, b2=b_2, bs=b_s)
-    # IA
-    pk_im = pt.get_pt_pk2d(ccl_cosmo, ptt_i, tracer2=ptt_m, ptc=ptc)
-    pk_ii = pt.get_pt_pk2d(ccl_cosmo, ptt_i, ptc=ptc)
-    pk_gi = pt.get_pt_pk2d(ccl_cosmo, ptt_g, tracer2=ptt_i, ptc=ptc)
-    # Galaxies
-    pk_gm = pt.get_pt_pk2d(ccl_cosmo, ptt_g, tracer2=ptt_m, ptc=ptc)
-    pk_gg = pt.get_pt_pk2d(ccl_cosmo, ptt_g, ptc=ptc)
-    # Magnification
+        # Code that computes effect from IA using that Pk2D object
+        t_lens = ccl.WeakLensingTracer(ccl_cosmo, dndz=(z, nz))
+        t_ia = ccl.WeakLensingTracer(ccl_cosmo, dndz=(z, nz), has_shear=False, ia_bias=(z, np.ones_like(z)), use_A_ia=False)
+        t_g = ccl.NumberCountsTracer(ccl_cosmo, has_rsd=False, dndz=(lens_z, lens_nz), bias=(lens_z, np.ones_like(lens_z)))
+        t_m = ccl.NumberCountsTracer(ccl_cosmo, has_rsd=False, dndz=(lens_z, lens_nz), bias=None, mag_bias=(lens_z, np.ones_like(lens_z)*mag_bias))
+        cl_GI = ccl.angular_cl(ccl_cosmo, t_lens, t_ia, ells, p_of_k_a=pk_im)*4.0
+        cl_II = ccl.angular_cl(ccl_cosmo, t_ia, t_ia, ells, p_of_k_a=pk_ii)*4.0
+        # The weak gravitational lensing power spectrum
+        cl_GG = ccl.angular_cl(ccl_cosmo, t_lens, t_lens, ells)*4.0
 
-    # Set the parameters for our systematics
+        # Galaxies
+        cl_gG = ccl.angular_cl(ccl_cosmo, t_g, t_lens, ells_ggl, p_of_k_a=pk_gm) * 6.0
+        cl_gI = ccl.angular_cl(ccl_cosmo, t_g, t_ia, ells_ggl, p_of_k_a=pk_gi) * 6.0
+        cl_gg = ccl.angular_cl(ccl_cosmo, t_g, t_g, ells_nc, p_of_k_a=pk_gg) * 9.0
+        # Magnification
+        cl_mI = 0.0#ccl.angular_cl(ccl_cosmo, t_m, t_ia, ells_ggl, p_of_k_a=pk_im)
+        cl_gm = 0.0#ccl.angular_cl(ccl_cosmo, t_g, t_m, ells_nc, p_of_k_a=pk_gm)
+        cl_mm = 0.0#ccl.angular_cl(ccl_cosmo, t_m, t_m, ells_nc)
+        cl_mG = 0.0#ccl.angular_cl(ccl_cosmo, t_m, t_lens, ells_ggl)
 
+        # The observed angular power spectrum is the sum of the two.
+        cl_cs_theory = cl_GG + 2*cl_GI + cl_II  # normally we would also have a third term, +cl_II).
+        #print(cl_GG, cl_GI, cl_II)
 
-    # Apply the systematics parameters
-    likelihood.update(systematics_params)
-    # Compute the log-likelihood, using the ccl.Cosmology object as the input
-    log_like = likelihood.compute_loglike(ccl_cosmo)
+        ang = ccl.correlation(ccl_cosmo, ells, cl_cs_theory, likelihood.statistics[0].ell_or_theta_/60, type="GG+")
+        ang_minus = ccl.correlation(ccl_cosmo, ells_minus, cl_cs_theory, likelihood.statistics[1].ell_or_theta_/60, type="GG-")
+       
+        ang_ggl = ccl.correlation(ccl_cosmo, ells_ggl, cl_gI+cl_gG+cl_mI + cl_mG, likelihood.statistics[2].ell_or_theta_/60, type="NG")
+        ang_nc = ccl.correlation(ccl_cosmo, ells_nc, cl_gg+2*cl_gm+cl_mm, likelihood.statistics[3].ell_or_theta_/60, type="NN")
+        end = time.time()
+        print(f"time to compute = {end-start: .3f}")
 
-    print(f"Log-like = {log_like:.1f}")
-
-    # Plot the predicted and measured statistic
-    x = likelihood.statistics[0].ell_or_theta_
-    x_ggl = likelihood.statistics[2].ell_or_theta_
-    x_nc = likelihood.statistics[3].ell_or_theta_
-    y_data = likelihood.statistics[0].measured_statistic_
-    y_err = np.sqrt(np.diag(likelihood.cov))[:len(x)]
-    y_theory = likelihood.statistics[0].predicted_statistic_
-    y_theory_ggl = likelihood.statistics[2].predicted_statistic_
-    y_theory_nc = likelihood.statistics[3].predicted_statistic_
-
-    print(likelihood.statistics)
-
-    #print(list(likelihood.statistics[0].cells.keys()))
-
-    ells = likelihood.statistics[0].ells
-    ells_ggl = likelihood.statistics[2].ells
-    ells_nc = likelihood.statistics[3].ells
-
-    # Code that computes effect from IA using that Pk2D object
-    t_lens = ccl.WeakLensingTracer(ccl_cosmo, dndz=(z, nz))
-    t_ia = ccl.WeakLensingTracer(ccl_cosmo, dndz=(z, nz), has_shear=False, ia_bias=(z, np.ones_like(z)), use_A_ia=False)
-    t_g = ccl.NumberCountsTracer(ccl_cosmo, has_rsd=False, dndz=(lens_z, lens_nz), bias=(lens_z, np.ones_like(lens_z)))
-    t_m = ccl.NumberCountsTracer(ccl_cosmo, has_rsd=False, dndz=(lens_z, lens_nz), bias=None, mag_bias=(lens_z, np.ones_like(lens_z)*0.0))
-    cl_GI = ccl.angular_cl(ccl_cosmo, t_lens, t_ia, ells, p_of_k_a=pk_im)
-    cl_II = ccl.angular_cl(ccl_cosmo, t_ia, t_ia, ells, p_of_k_a=pk_ii)
-    # The weak gravitational lensing power spectrum
-    cl_GG = ccl.angular_cl(ccl_cosmo, t_lens, t_lens, ells)
-
-    # Galaxies
-    cl_gG = ccl.angular_cl(ccl_cosmo, t_g, t_lens, ells_ggl, p_of_k_a=pk_gm)
-    cl_gI = ccl.angular_cl(ccl_cosmo, t_g, t_ia, ells_ggl, p_of_k_a=pk_gi)
-    cl_gg = ccl.angular_cl(ccl_cosmo, t_g, t_g, ells_nc, p_of_k_a=pk_gg)
-    # Magnification
-    cl_mI = 0.0#ccl.angular_cl(ccl_cosmo, t_m, t_ia, ells_ggl, p_of_k_a=pk_im)
-    cl_gm = 0.0#ccl.angular_cl(ccl_cosmo, t_g, t_m, ells_nc, p_of_k_a=pk_gm)
-
-    # The observed angular power spectrum is the sum of the two.
-    cl_cs_theory = cl_GG + 2*cl_GI + cl_II  # normally we would also have a third term, +cl_II).
-
-
-    sources[f"src{i}"] = wl.WeakLensingPT(
-        sacc_tracer=f"src{i}",
-        systematics=[ia_systematic, pzshift]
-    )
-    
-    #t_ia_pt = pt.WeakLensingPT(ccl_cosmo, )
-    #t_ia_pt = ccl.
-    #print(t_m.get_kernel(0.5))
-    ang = ccl.correlation(ccl_cosmo, ells, cl_cs_theory, likelihood.statistics[0].ell_or_theta_/60, type="GG+")
-    ang_ggl = ccl.correlation(ccl_cosmo, ells_ggl, cl_gI+cl_gG+cl_mI, likelihood.statistics[2].ell_or_theta_/60, type="NG")
-    ang_nc = ccl.correlation(ccl_cosmo, ells_nc, cl_gg+cl_gm, likelihood.statistics[3].ell_or_theta_/60, type="NN")
     fig, ax = plt.subplots(2, 1, sharex=True, figsize=(6, 6))
     fig.subplots_adjust(hspace=0)
     # ax[0].plot(x, y_theory, label="Total")
@@ -263,11 +256,13 @@ if __name__ == "__main__":
     #ax[0].plot(ells, cells_cs_total, label="total CS firecrown")
     #ax[0].plot(ells, cl_cs_theory, ls="--", label="total CS CCL")
 
-    #ax[0].plot(x, y_theory, label="my code xi_+")
-    #ax[0].plot(x_ggl, y_theory_ggl, label="my code ggl")
+    ax[0].plot(x, y_theory, label="my code xi_+")
+    ax[0].plot(x_minus, y_theory_minus, label="my code xi_-")
+    ax[0].plot(x_ggl, y_theory_ggl, label="my code ggl")
     ax[0].plot(x_nc, y_theory_nc, label="my code nc")
-    #ax[0].plot(x, ang, label="direct calculation xi_+")
-    #ax[0].plot(x_ggl, ang_ggl, label="direct calculation ggl")
+    ax[0].plot(x, ang, label="direct calculation xi_+")
+    ax[0].plot(x_minus, ang_minus, label="direct calculation xi_+")
+    ax[0].plot(x_ggl, ang_ggl, label="direct calculation ggl")
     ax[0].plot(x_nc, ang_nc, label="direct calculation nc")
 
     #ax[0].errorbar(x, y_data, y_err, ls="none", marker="o")
@@ -277,8 +272,9 @@ if __name__ == "__main__":
     ax[0].set_ylabel(r"$\xi_+(\theta)$")
     ax[0].legend()
 
-    #ax[1].plot(x, (y_theory-ang)/ang, label="xi_+ residual")
-    #ax[1].plot(x_ggl, (y_theory_ggl-ang_ggl)/ang_ggl, label="ggl residual")
+    ax[1].plot(x, (y_theory-ang)/ang, label="xi_+ residual")
+    ax[1].plot(x_minus, (y_theory_minus-ang_minus)/ang_minus, label="xi_- residual")
+    ax[1].plot(x_ggl, (y_theory_ggl-ang_ggl)/ang_ggl, label="ggl residual")
     ax[1].plot(x_nc, (y_theory_nc-ang_nc)/ang_nc, label="nc residual")
 
     ax[1].set_xlabel(r"$\theta$ [arcmin]")

firecrown/likelihood/gauss_family/statistic/source/number_counts.py
@@ -45,7 +45,6 @@ class NumberCountsPTArgs:
     bias: np.ndarray
     bias_2: np.ndarray
     bias_s: np.ndarray
-    mag_bias: np.ndarray
 
 class NumberCountsSystematic(Systematic):
     """Class implementing systematics for Number Counts sources."""
@@ -356,7 +355,7 @@ class NLBiasSystematic(NumberCountsPTSystematic):
             bias=tracer_arg.bias * np.ones_like(tracer_arg.z),
             bias_2=tracer_arg.bias_2 * np.ones_like(tracer_arg.z),
             bias_s=tracer_arg.bias_s * np.ones_like(tracer_arg.z),
-            mag_bias=tracer_arg.mag_bias,
+            #mag_bias=tracer_arg.mag_bias,
         )
 
 
@@ -653,7 +652,7 @@ class NumberCountsPT(SourcePT):
         z = z[inds]
         nz = nz[inds]
 
-        self.pttracer_args = NumberCountsPTArgs(scale=self.scale, z=z, dndz=nz, bias=None, bias_2=None, bias_s=None,mag_bias=None)
+        self.pttracer_args = NumberCountsPTArgs(scale=self.scale, z=z, dndz=nz, bias=None, bias_2=None, bias_s=None)
         self.tracer_args = NumberCountsArgs(scale=self.scale, z=z, dndz=nz, bias=None, mag_bias=None)
         self.pt_tracer_args = NumberCountsArgs(scale=self.scale, z=z, dndz=nz, bias=None, mag_bias=None)
 
@@ -723,20 +722,9 @@ class NumberCountsPT(SourcePT):
             bias=bias,
             bias_2 = bias_2,
             bias_s=bias_s,
-            mag_bias=pttracer_args.mag_bias,
+            #mag_bias=pttracer_args.mag_bias,
         )
 
-        if self.mag_bias is not None:
-            mag_bias = np.ones_like(pttracer_args.z) * self.mag_bias
-            pttracer_args = NumberCountsPTArgs(
-                scale=pttracer_args.scale,
-                z=pttracer_args.z,
-                dndz=pttracer_args.dndz,
-                bias=pttracer_args.bias,
-                bias_2 = pttracer_args.bias_2,
-                bias_s=pttracer_args.bias_s,
-                mag_bias=mag_bias,
-            )
 
         for systematic in self.systematics:
             if  isinstance(systematic,NumberCountsPTSystematic):

firecrown/likelihood/gauss_family/statistic/two_point.py
@@ -12,6 +12,7 @@ import scipy.interpolate
 
 import pyccl
 import pyccl.nl_pt
+import time
 
 from .statistic import Statistic
 from .source.source import Source, Systematic, SourcePT
@@ -57,6 +58,14 @@ def _generate_ell_or_theta(*, minimum, maximum, n, binning="log"):
 @functools.lru_cache(maxsize=128)
 def _cached_angular_cl(cosmo, tracers, ells, p_of_k_a=None):
     return pyccl.angular_cl(cosmo, *tracers, np.array(ells), p_of_k_a=p_of_k_a)
+
+@functools.lru_cache(maxsize=128)
+def _cached_pk(cosmo, tracers, ptc_calc):
+    return pyccl.nl_pt.get_pt_pk2d(cosmo, *tracers, ptc=ptc_calc)
+
+@functools.lru_cache(maxsize=128)
+def _cached_matter_tracer(cosmo):
+    return pyccl.nl_pt.PTMatterTracer()
 
 
 class TwoPoint(Statistic):
@@ -126,6 +135,13 @@ class TwoPoint(Statistic):
            logarithmically spaced between `mid` and `max`. Default is 6e4.
          - n_log : int, optional - The number of logarithmically spaced angular
            wavenumber samples between `mid` and `max`. Default is 200.
+    pt_calc : PT Calculator, required when using PT Sources
+    calc_hm_1, calc_hm_2: int, optional
+        An integer indicating the calculation of the Halo Model Power Spectrum, one to be set for each tracer
+        0 : No calculation, use linear theory
+        1 : Calculate the 1-halo term only
+        2 : Calculate the 2-halo term only
+        3 : Calculate both the 1- and 2-halo terms
 
     Attributes
     ----------
@@ -159,6 +175,8 @@ class TwoPoint(Statistic):
         ell_or_theta_min=None,
         ell_or_theta_max=None,
         pt_calc: PTCalculator = None,
+        calc_hm_1= 0,
+        calc_hm_2= 0,
     ):
         super().__init__()
 
@@ -166,6 +184,8 @@ class TwoPoint(Statistic):
         assert isinstance(source1, Source)
 
         self.pt_calc=pt_calc
+        self.calc_hm_1 = calc_hm_1
+        self.calc_hm_2 = calc_hm_2
 
         if (self.pt_calc==None and ((isinstance(source0, SourcePT)) or (isinstance(source1, SourcePT)) )):
             raise ValueError(
@@ -308,7 +328,14 @@ class TwoPoint(Statistic):
             self.ells = _ell_for_xi(**self.ell_for_xi)
         self.cells = {}
         
-        print(isinstance(self.source0, SourcePT),isinstance(self.source1, SourcePT))
+        #print(isinstance(self.source0, SourcePT),isinstance(self.source1, SourcePT))
+        #print(self.source0, self.source1)
+
+        if (self.calc_hm_1 or self.calc_hm_2):
+            raise NotImplementedError(
+                        "Halo model power spectra not supported yet"
+                    )
+
 
         if (((not isinstance(self.source0, SourcePT)) and (not isinstance(self.source1, SourcePT)))):
 
@@ -337,81 +364,55 @@ class TwoPoint(Statistic):
 
         elif (((isinstance(self.source0, SourcePT)) and (not isinstance(self.source1, SourcePT))) or ((isinstance(self.source1, SourcePT)) and (not isinstance(self.source0, SourcePT))) ):
             #changing notation such that source0 is always the SourcePT object
-            print("testing swap", isinstance(self.source0, SourcePT), isinstance(self.source1, SourcePT))
-            if isinstance(self.source0, SourcePT):
-
-                ptt_m = pyccl.nl_pt.PTMatterTracer()
-
-                tracers0 = self.source0.get_tracer(cosmo)
-                tracers1 = self.source1.get_tracer(cosmo)
-                scale0 = self.source0.get_scale()
-                scale1 = self.source1.get_scale()
-
-                pt_tracers0 = self.source0.get_pt_tracer(cosmo)
-                pt_scale0 = self.source0.get_pt_scale()
-
-            
-                pttracers0 = self.source0.get_pttracer(cosmo)
-                ptscale0 = self.source0.get_ptscale()
-
-                withNC = False
-                withIA = False
-                if (pttracers0.type == 'NC'): withNC=True
-                if (pttracers0.type == 'IA'): withIA=True
-                #ptc = self.pt_calc(with_NC=withNC, with_IA=withIA,
-                #          log10k_min=-4, log10k_max=2, nk_per_decade=20)
-                pk_0m = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers0, tracer2=ptt_m, ptc=self.pt_calc)
+            if isinstance(self.source1, SourcePT):
+                temp_source = self.source0
+                self.source0 = self.source1
+                self.source1 = temp_source
 
 
-                
-                theory_vector = np.array(pyccl.angular_cl(cosmo, pt_tracers0, tracers1, self.ells, p_of_k_a=pk_0m))*pt_scale0*ptscale0*scale1
-                theory_vector += np.array(pyccl.angular_cl(cosmo, tracers0, tracers1, self.ells))*scale0*scale1       
+            ptt_m = _cached_matter_tracer(cosmo)
 
-                if not self.ccl_kind == "cl":
-                    theory_vector = (
-                        pyccl.correlation(
-                            cosmo, self.ells, theory_vector, self.ell_or_theta_ / 60, type=self.ccl_kind
-                        )
-                    )
-            else:
+            tracers0 = self.source0.get_tracer(cosmo)
+            tracers1 = self.source1.get_tracer(cosmo)
+            scale0 = self.source0.get_scale()
+            scale1 = self.source1.get_scale()
 
-                ptt_m = pyccl.nl_pt.PTMatterTracer()
-
-                tracers1 = self.source1.get_tracer(cosmo)
-                tracers0 = self.source0.get_tracer(cosmo)
-                scale1 = self.source1.get_scale()
-                scale0 = self.source0.get_scale()
-
-                pt_tracers1 = self.source1.get_pt_tracer(cosmo)
-                pt_scale1 = self.source1.get_pt_scale()
+            pt_tracers0 = self.source0.get_pt_tracer(cosmo)
+            pt_scale0 = self.source0.get_pt_scale()
 
             
-                pttracers1 = self.source1.get_pttracer(cosmo)
-                ptscale1 = self.source1.get_ptscale()
+            pttracers0 = self.source0.get_pttracer(cosmo)
+            ptscale0 = self.source0.get_ptscale()
+            
+            #equivalent to pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers0, tracer2=ptt_m, ptc=self.pt_calc)
+            pk_0m = _cached_pk(cosmo, (pttracers0, ptt_m), self.pt_calc)
 
-                withNC = False
-                withIA = False
-                if (pttracers1.type == 'NC'): withNC=True
-                if (pttracers1.type == 'IA'): withIA=True
-                #ptc = self.pt_calc(with_NC=withNC, with_IA=withIA,
-                #          log10k_min=-4, log10k_max=2, nk_per_decade=20)
-                pk_1m = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers1, tracer2=ptt_m, ptc=self.pt_calc)
+            
+            #equivalent to theory_vector = np.array(pyccl.angular_cl(cosmo, pt_tracers0, tracers1, self.ells, p_of_k_a=pk_0m))*pt_scale0*ptscale0*scale1
 
-
-                
-                theory_vector = np.array(pyccl.angular_cl(cosmo, pt_tracers1, tracers0, self.ells, p_of_k_a=pk_1m))*pt_scale1*ptscale1*scale0
-                theory_vector += np.array(pyccl.angular_cl(cosmo, tracers1, tracers0, self.ells))*scale0*scale1
-
-                if not self.ccl_kind == "cl":
-                    theory_vector = (
-                        pyccl.correlation(
-                            cosmo, self.ells, theory_vector, self.ell_or_theta_ / 60, type=self.ccl_kind
+            theory_vector = (                        
+                        _cached_angular_cl(
+                            cosmo, (pt_tracers0, tracers1), tuple(self.ells), p_of_k_a=pk_0m)
+                        )*pt_scale0*ptscale0*scale1
+            #equivalent to theory_vector += np.array(pyccl.angular_cl(cosmo, tracers0, tracers1, self.ells))*scale0*scale1  
+            theory_vector += (                        
+                        _cached_angular_cl(
+                            cosmo, (tracers0, tracers1), tuple(self.ells)
                         )
+                        *scale0*scale1
                     )
+
+            if not self.ccl_kind == "cl":
+                theory_vector = (
+                    pyccl.correlation(
+                        cosmo, self.ells, theory_vector, self.ell_or_theta_ / 60, type=self.ccl_kind
+                    )
+                )
+
         else:
+            ptt_m = _cached_matter_tracer(cosmo)
 
-            ptt_m = pyccl.nl_pt.PTMatterTracer()
-
+            #print(self.source0, self.source1, ptt_m)
 
             tracers0 = self.source0.get_tracer(cosmo)
             tracers1 = self.source1.get_tracer(cosmo)
@@ -429,22 +430,34 @@ class TwoPoint(Statistic):
             ptscale0 = self.source0.get_ptscale()
             ptscale1 = self.source1.get_ptscale()
 
-            withNC = False
-            withIA = False
-            if (pttracers0.type == 'NC' or pttracers1.type == 'NC'): withNC=True
-            if (pttracers0.type == 'IA' or pttracers1.type == 'IA'): withIA=True
-            #ptc = self.pt_calc(with_NC=withNC, with_IA=withIA,
-            #          log10k_min=-4, log10k_max=2, nk_per_decade=20)
-            pk_0m = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers0, tracer2=ptt_m, ptc=self.pt_calc)
-            pk_1m = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers1, tracer2=ptt_m, ptc=self.pt_calc)
-            pk_01 = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers0, pttracers1, ptc=self.pt_calc)
+            start = time.time()
+            print(_cached_matter_tracer.cache_info().hits,_cached_pk.cache_info().hits, _cached_angular_cl.cache_info().hits)
+            pk_0m = _cached_pk(cosmo, (pttracers0, ptt_m), self.pt_calc) 
+            pk_1m = _cached_pk(cosmo, (pttracers1, ptt_m), self.pt_calc)
+            pk_01 = _cached_pk(cosmo, (pttracers0, pttracers1), self.pt_calc)
+            end = time.time()
+            print(_cached_matter_tracer.cache_info().hits,_cached_pk.cache_info().hits, _cached_angular_cl.cache_info().hits, end-start)
 
-            #if self.ccl_kind == "cl":
+            #pk_0m = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers0, tracer2=ptt_m, ptc=self.pt_calc)
+            #pk_1m = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers1, tracer2=ptt_m, ptc=self.pt_calc)
+            #pk_01 = pyccl.nl_pt.get_pt_pk2d(cosmo, pttracers0, pttracers1, ptc=self.pt_calc)
 
-            theory_vector = np.array(pyccl.angular_cl(cosmo, pt_tracers0, pt_tracers1, self.ells, p_of_k_a=pk_01))*pt_scale0*ptscale0*pt_scale1*ptscale1
-            theory_vector += np.array(pyccl.angular_cl(cosmo, pt_tracers0, tracers1, self.ells, p_of_k_a=pk_0m))*pt_scale0*ptscale0*scale1
-            theory_vector += np.array(pyccl.angular_cl(cosmo, pt_tracers1, tracers0, self.ells, p_of_k_a=pk_1m))*pt_scale1*ptscale1*scale0
-            theory_vector += np.array(pyccl.angular_cl(cosmo, tracers0, tracers1, self.ells))*scale0*scale1
+
+            theory_vector = (_cached_angular_cl(cosmo, (pt_tracers0, pt_tracers1), tuple(self.ells), p_of_k_a=pk_01))*pt_scale0*pt_scale1
+            theory_vector_prime = theory_vector.copy()
+
+            theory_vector+=(_cached_angular_cl(cosmo, (pt_tracers0, tracers1), tuple(self.ells), p_of_k_a=pk_0m))*pt_scale0*scale1
+            theory_vector+=(_cached_angular_cl(cosmo, (pt_tracers1, tracers0), tuple(self.ells), p_of_k_a=pk_1m))*pt_scale1*scale0
+            theory_vector+=(_cached_angular_cl(cosmo, (tracers0, tracers1), tuple(self.ells))
+                        *scale0*scale1)
+            #print(theory_vector-theory_vector_prime, (tracers1._trc))
+
+            print(_cached_matter_tracer.cache_info().hits,_cached_pk.cache_info().hits, _cached_angular_cl.cache_info().hits)
+            print("scales", scale1, pt_scale1, ptscale1, scale0, pt_scale0, ptscale0)
+            #theory_vector = np.array(pyccl.angular_cl(cosmo, pt_tracers0, pt_tracers1, self.ells, p_of_k_a=pk_01))*pt_scale0*ptscale0*pt_scale1*ptscale1
+            #theory_vector += np.array(pyccl.angular_cl(cosmo, pt_tracers0, tracers1, self.ells, p_of_k_a=pk_0m))*pt_scale0*ptscale0*scale1
+            #theory_vector += np.array(pyccl.angular_cl(cosmo, pt_tracers1, tracers0, self.ells, p_of_k_a=pk_1m))*pt_scale1*ptscale1*scale0
+            #theory_vector += np.array(pyccl.angular_cl(cosmo, tracers0, tracers1, self.ells))*scale0*scale1
 
 
             if not self.ccl_kind == "cl":
@@ -453,7 +466,6 @@ class TwoPoint(Statistic):
                         cosmo, self.ells, theory_vector, self.ell_or_theta_ / 60, type=self.ccl_kind
                     )
                 )
-
 
 
         if self.theory_window_function is not None: