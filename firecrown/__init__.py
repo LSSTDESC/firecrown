b5b7185aae47c59e3ed37dc2ea1005c64857a3d5
Marc Paterno <paterno@fnal.gov>
2022-12-28 13:59:19 -0600

    Simplify getting version

--- firecrown/__init__.py
+++ firecrown/__init__.py
@@ -5,4 +5,5 @@ A package for creation of DESC likelihoods.
 
 """
 
+from .version import __version__
 # flake8: noqa
---
+++ tests/test_version.py
@@ -0,0 +1,4 @@
+import firecrown
+
+def test_version():
+    assert firecrown.__version__ == "1.3.0"
