d28baca5a93beaa0572328738b5e464652b656c5
Marc Paterno <paterno@fnal.gov>
2023-01-07 13:38:58 -0600

    Add upper_triangle_indices function

---
+++ firecrown/utils.py
@@ -0,0 +1,15 @@
+"""Some utility functions for patterns common in Firecrown.
+"""
+
+
+def upper_triangle_indices(n):
+    """generator that yields a sequence of tuples that carry the indices for an
+    upper-triangular matrix. This is a replacement for the nested loops:
+
+    for i in range(n):
+      for j in range(i, n):
+        ...
+    """
+    for i in range(n):
+        for j in range(i, n):
+            yield i, j
