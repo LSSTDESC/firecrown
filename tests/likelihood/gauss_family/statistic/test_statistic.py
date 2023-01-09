74f5ad08bed526edb9893860469ed6458d3a27f7
Marc Paterno <paterno@fnal.gov>
2023-01-09 08:33:59 -0600

    Additional testing for DataVector and TheoryVectoy

--- tests/likelihood/gauss_family/statistic/test_statistic.py
+++ tests/likelihood/gauss_family/statistic/test_statistic.py
@@ -2,12 +2,15 @@ import numpy as np
 import firecrown.likelihood.gauss_family.statistic.statistic as stat
 
 
+VECTOR_CLASSES = (stat.TheoryVector, stat.DataVector)
+
+
 def test_vector_create():
     vals = np.random.random_sample((10,))
     assert isinstance(vals, np.ndarray)
     assert vals.dtype == np.float64
     assert vals.shape == (10,)
-    for cls in (stat.TheoryVector, stat.DataVector):
+    for cls in VECTOR_CLASSES:
         result = cls.create(vals)
         assert isinstance(result, cls)
         assert result.shape == (10,)
@@ -18,7 +21,7 @@ def test_vector_from_list():
     vals = [1.5, 2.5, -3.0, 10.0]
     assert isinstance(vals, list)
     assert len(vals) == 4
-    for cls in (stat.TheoryVector, stat.DataVector):
+    for cls in VECTOR_CLASSES:
         result = cls.from_list(vals)
         assert isinstance(result, cls)
         assert result.shape == (4,)
@@ -27,7 +30,7 @@ def test_vector_from_list():
 
 
 def test_vector_slicing():
-    for cls in (stat.TheoryVector, stat.DataVector):
+    for cls in VECTOR_CLASSES:
         vec = cls.create(np.random.random_sample((12,)))
         assert isinstance(vec, cls)
         middle_part = vec[3:6]
@@ -36,10 +39,29 @@ def test_vector_slicing():
 
 
 def test_vector_copying():
-    for cls in (stat.TheoryVector, stat.DataVector):
+    for cls in VECTOR_CLASSES:
         vec = cls.create(np.random.random_sample((12,)))
         assert isinstance(vec, cls)
         vec_copy = vec.copy()
         assert vec_copy is not vec
         assert np.array_equal(vec, vec_copy)
         assert isinstance(vec_copy, cls)
+
+
+def test_excplicit_vector_construction():
+    for cls in VECTOR_CLASSES:
+        vec = cls(shape = (4,), dtype = np.float64)
+        assert isinstance(vec, cls)
+        assert vec.shape == (4,)
+        assert vec.dtype == np.float64
+
+
+def test_ufunc_on_vector():
+    data = np.array([0., 0.25, 0.50])
+    expected = np.sin(data)
+    for cls in VECTOR_CLASSES:
+        vec = cls.create(data)
+        result = np.sin(vec)
+        assert isinstance(result, cls)
+        assert np.array_equal(result, expected)
+
