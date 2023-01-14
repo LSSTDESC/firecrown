4231b4850ed1eed78a5a28cf676dda13b0ad6689
Marc Paterno <paterno@fnal.gov>
2023-01-14 13:46:32 -0600

    Test some required pyccl behavior

---
+++ tests/test_pyccl.py
@@ -0,0 +1,41 @@
+import pytest
+import pyccl
+
+
+# Both sets of cosmological parameters are silly, but they are sufficient to initialize
+# a pyccl.Cosmology object.
+
+
+@pytest.fixture(name="cosmo_params_1")
+def fixture_cosmo_params_1():
+    return {"Omega_c": 0.0, "Omega_b": 0.0, "h": 1.0, "A_s": 0.0, "n_s": 0.0}
+
+
+@pytest.fixture(name="cosmo_params_2")
+def fixture_cosmo_params_2():
+    return {"Omega_c": 0.0, "Omega_b": 0.0, "h": 1.0, "A_s": 0.0, "n_s": 0.25}
+
+
+def test_alias_of_cosmology_hashes_equal(cosmo_params_1):
+    x = pyccl.Cosmology(**cosmo_params_1)
+    y = x
+    assert x == y
+    assert hash(x) == hash(y)
+
+
+def test_unequal_cosmologies_hash_unequal(cosmo_params_1, cosmo_params_2):
+    x = pyccl.Cosmology(**cosmo_params_1)
+    y = pyccl.Cosmology(**cosmo_params_2)
+    assert x != y
+    assert hash(x) != hash(y)
+
+
+def test_equal_cosmologies_hash_equal(cosmo_params_1):
+    # This test verifies expected (but not really desired) behavior. Two
+    # Cosmology variables only have equal hashes when they are in fact they
+    # are aliases for the same object. They also test as equal only if they
+    # are aliases for the same object.
+    x = pyccl.Cosmology(**cosmo_params_1)
+    y = pyccl.Cosmology(**cosmo_params_1)
+    assert x != y
+    assert hash(x) != hash(y)
