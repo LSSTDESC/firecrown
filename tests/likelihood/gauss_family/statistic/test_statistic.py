2be813f38b3fa2652eb3249948006f1e5ed39b48
Marc Paterno <paterno@fnal.gov>
2023-03-02 09:49:50 -0600

    233 more clean up in srd sn and cosmicshear examples (#234)
    * Cleanup for pylint
    * Simplify cosmicshear example code
    * mypy does not understand this type usage
    * Refactor for clarity
    * Run mypy on examples
    * Run CI on pull requests and pushes directly to master

--- tests/likelihood/gauss_family/statistic/test_statistic.py
+++ tests/likelihood/gauss_family/statistic/test_statistic.py
@@ -1,7 +1,6 @@
 import numpy as np
 import firecrown.likelihood.gauss_family.statistic.statistic as stat
 
-
 VECTOR_CLASSES = (stat.TheoryVector, stat.DataVector)
 
 
@@ -11,7 +10,7 @@ def test_vector_create():
     assert vals.dtype == np.float64
     assert vals.shape == (10,)
     for cls in VECTOR_CLASSES:
-        result = cls.create(vals)
+        result = cls.create(vals)  # type: ignore
         assert isinstance(result, cls)
         assert result.shape == (10,)
         assert np.array_equal(vals, result)
@@ -22,7 +21,7 @@ def test_vector_from_list():
     assert isinstance(vals, list)
     assert len(vals) == 4
     for cls in VECTOR_CLASSES:
-        result = cls.from_list(vals)
+        result = cls.from_list(vals)  # type: ignore
         assert isinstance(result, cls)
         assert result.shape == (4,)
         for i, val in enumerate(vals):
@@ -31,7 +30,7 @@ def test_vector_from_list():
 
 def test_vector_slicing():
     for cls in VECTOR_CLASSES:
-        vec = cls.create(np.random.random_sample((12,)))
+        vec = cls.create(np.random.random_sample((12,)))  # type: ignore
         assert isinstance(vec, cls)
         middle_part = vec[3:6]
         assert middle_part.shape == (3,)
@@ -40,7 +39,7 @@ def test_vector_slicing():
 
 def test_vector_copying():
     for cls in VECTOR_CLASSES:
-        vec = cls.create(np.random.random_sample((12,)))
+        vec = cls.create(np.random.random_sample((12,)))  # type: ignore
         assert isinstance(vec, cls)
         vec_copy = vec.copy()
         assert vec_copy is not vec
@@ -50,7 +49,7 @@ def test_vector_copying():
 
 def test_excplicit_vector_construction():
     for cls in VECTOR_CLASSES:
-        vec = cls(shape=(4,), dtype=np.float64)
+        vec = cls(shape=(4,), dtype=np.float64)  # type: ignore
         assert isinstance(vec, cls)
         assert vec.shape == (4,)
         assert vec.dtype == np.float64
@@ -60,7 +59,7 @@ def test_ufunc_on_vector():
     data = np.array([0.0, 0.25, 0.50])
     expected = np.sin(data)
     for cls in VECTOR_CLASSES:
-        vec = cls.create(data)
+        vec = cls.create(data)  # type: ignore
         result = np.sin(vec)
         assert isinstance(result, cls)
         assert np.array_equal(result, expected)
