5e8d67218e9e7d7e9050dfe9d3c0100bc3070ff4
Marc Paterno <paterno@fnal.gov>
2023-05-23 12:36:28 -0500

    Add doctring to fixture

--- tests/likelihood/test_cosmosis_likelihood.py
+++ tests/likelihood/test_cosmosis_likelihood.py
@@ -6,6 +6,7 @@ import firecrown.connector.cosmosis.likelihood as like
 
 @fixture(name="config")
 def make_config() -> DataBlock:
+    """Return a DataBlock with a minimal configuration."""
     db = DataBlock()
     db.put("module_options", "firecrown_config", "tests/likelihood/lkdir/lkscript.py")
     return db
