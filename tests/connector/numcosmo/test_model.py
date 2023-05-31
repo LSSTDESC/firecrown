b5791ee78bbcc6fe333aa5b72f8ec178f1da0258
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2023-05-31 18:09:03 -0300

    Testing saving and loading models.

--- tests/connector/numcosmo/test_model.py
+++ tests/connector/numcosmo/test_model.py
@@ -1,6 +1,7 @@
 """Unit tests for the numcosmo model connector."""
 
 import pytest
+import yaml
 from firecrown.connector.numcosmo.model import (
     NumCosmoModel,
     ScalarParameter,
@@ -46,6 +47,7 @@ def test_parameterless_module_construction(model):
     for i, sparam in enumerate(model.scalar_params):
         assert m.param_name(i) == sparam.name
         assert m.param_symbol(i) == sparam.symbol
+        assert m.param_get(i) == sparam.default_value
         assert m.param_get_scale(i) == sparam.scale
         assert m.param_get_lower_bound(i) == sparam.lower_bound
         assert m.param_get_upper_bound(i) == sparam.upper_bound
@@ -55,7 +57,27 @@ def test_parameterless_module_construction(model):
         for j in range(vparam.default_length):
             assert m.param_name(i) == f"{vparam.name}_{j}"
             assert m.param_symbol(i) == f"{{{vparam.symbol}}}_{j}"
+            assert m.param_get(i) == vparam.default_value
             assert m.param_get_scale(i) == vparam.scale
             assert m.param_get_lower_bound(i) == vparam.lower_bound
             assert m.param_get_upper_bound(i) == vparam.upper_bound
             i += 1
+
+
+def test_model_save_load(tmp_path, model):
+    """Test saving and loading a NumCosmo model."""
+
+    with open(
+        tmp_path / r"numcosmo_firecrown_model.yml", "w", encoding="utf8"
+    ) as modelfile:
+        yaml.dump(model, modelfile, Dumper=yaml.Dumper)
+
+    with open(
+        tmp_path / r"numcosmo_firecrown_model.yml", "r", encoding="utf8"
+    ) as modelfile:
+        model_copy = yaml.load(modelfile, Loader=yaml.Loader)
+
+    assert model_copy.name == model.name
+    assert model_copy.description == model.description
+    assert model_copy.scalar_params == model.scalar_params
+    assert model_copy.vector_params == model.vector_params
