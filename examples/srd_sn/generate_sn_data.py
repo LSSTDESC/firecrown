8676d4a708f8928de6fffd4d2b8f7e52f759b12b
Ayan Mitra <ayanmitra375@gmail.com>
2023-06-15 00:41:20 -0700

    generate_sn_data.py code edit

--- examples/srd_sn/generate_sn_data.py
+++ examples/srd_sn/generate_sn_data.py
@@ -17,7 +17,7 @@ from firecrown.sacc_support import sacc
 S = Sacc()
 
 
-def conversion(h):
+def conversion(hdg):
     """
     CODE TO CONVERT THE NEW DESC HUBBLE DIAGRAM FILE STRUCTURE
     SIMILAR TO COSMOMC STYLED INPUT HUBBLE DIAGRAM
@@ -44,49 +44,48 @@ def conversion(h):
         "dec",
         "biascor",
     ]
-    h = pd.DataFrame(y1dat)
-    h.columns = h.iloc[0, :]
-    h = h.iloc[1:, 1:-1]
-    h = h.apply(pd.to_numeric)
-    h.MU -= 19.36
-    h.insert(3, "dz", np.zeros(np.shape(h)[0]))
-    row = np.shape(h)[0]
+    hub = pd.DataFrame(hdg)
+    hub.columns = hub.iloc[0, :]
+    hub = hub[
+        ["ROW", "zCMB", "zHEL", "MU", "MUERR"]
+    ]  # takes only "ROW,zCMB,zHEL,MU,MUERR" cols
+    hub = hub.iloc[1:, :]
+    hub = hub.apply(pd.to_numeric)
+    hub.MU -= 19.36
+    hub.insert(3, "dz", np.zeros(np.shape(hub)[0]))
+    row = np.shape(hub)[0]
     colu = 13
     join = np.zeros((row, colu))
-    hh = pd.DataFrame(np.concatenate([h, join], axis=1))
-    if np.shape(hh)[1] < 19:
-        diff = int(19 - np.shape(hh)[1])
-        for i in reversed(range(diff)):
-            hh.insert(np.shape(hh)[1], " ", np.zeros(np.shape(hh)[0]))
-    hh.columns = col
-    h = hh
-    h["#name"] = np.linspace(0, np.shape(h)[0] - 1, np.shape(h)[0]).astype(int)
-    h = np.array(h.T)
-    return h
+    hhub = pd.DataFrame(np.concatenate([hub, join], axis=1))
+    hhub.columns = col
+    hub = hhub
+    hub["#name"] = np.linspace(0, np.shape(hub)[0] - 1, np.shape(hub)[0]).astype(int)
+    hub = np.array(hub.T)
+    return hub
 
 
 if len(sys.argv) == 4:
-    path = sys.argv[1]
+    PATH = sys.argv[1]
     HD = sys.argv[2]
     cov = sys.argv[3]
-    y1dat = np.loadtxt(open(path + "/" + HD, "rt"), comments="#", dtype=str)
-    y1cov = np.loadtxt(path + "/" + cov, comments="#", unpack=True)
+    y1dat = np.loadtxt(open(PATH + "/" + HD, "rt"), comments="#", dtype=str)
+    y1cov = np.loadtxt(PATH + "/" + cov, comments="#", unpack=True)
     if (y1dat[0][0][0]).isnumeric():
         y1dat = np.array(y1dat).astype(float).T
     else:
         y1dat = conversion(y1dat)
 else:
-    dirname_year1 = "sndata/Y1_DDF_FOUNDATION"
-    dirname_year10 = "sndata/Y10_DDF_WFD_FOUNDATION/"
-    url = "https://zenodo.org/record/2662127/files/LSST_DESC_SRD_v1_release.tar.gz?download=1"
+    DIRNAME_YEAR1 = "sndata/Y1_DDF_FOUNDATION"
+    DIRNAME_YEAR10 = "sndata/Y10_DDF_WFD_FOUNDATION/"
+    URL = "https://zenodo.org/record/2662127/files/LSST_DESC_SRD_v1_release.tar.gz?download=1"
 
     # check if file exists
-    if os.path.exists(dirname_year1):
+    if os.path.exists(DIRNAME_YEAR1):
         print("Y1 directory already downloaded")
     else:
         print("Downloading full DESC SRD release files")
         os.mkdir("sndata")
-        urllib.request.urlretrieve(url, "sndata/LSST_DESC_SRD_v1_release.tar.gz")
+        urllib.request.urlretrieve(URL, "sndata/LSST_DESC_SRD_v1_release.tar.gz")
         os.chdir("./sndata/")
         print("Extracting full DESC SRD release files")
         tf = tarfile.open("LSST_DESC_SRD_v1_release.tar.gz")
@@ -102,12 +101,12 @@ else:
         os.chdir("../../")
         print("Done")
 
-        if os.path.exists(dirname_year10):
+        if os.path.exists(DIRNAME_YEAR10):
             print("Y10 directory already downloaded")
         else:
             print("Downloading full DESC SRD release files")
             os.mkdir("sndata")
-            urllib.request.urlretrieve(url, "sndata/LSST_DESC_SRD_v1_release.tar.gz")
+            urllib.request.urlretrieve(URL, "sndata/LSST_DESC_SRD_v1_release.tar.gz")
             os.chdir("./sndata/")
             print("Extracting full DESC SRD release files")
             tf = tarfile.open("LSST_DESC_SRD_v1_release.tar.gz")
@@ -169,8 +168,8 @@ dmb = y1dat[5]
 zmu = np.vstack((zcmb, mb))
 size = int(y1cov[0])  # reading the size of the matrix from the first entry
 covmat = np.zeros((size, size), dtype=float)
-count = 1  # since the cov mat starts with the number of lines
-out_name = "srd-y1-converted"
+COUNT = 1  # since the cov mat starts with the number of lines
+OUT_NAME = "srd-y1-converted"
 
 S.add_tracer("misc", "sn_ddf_sample")
 
@@ -180,8 +179,8 @@ for i in range(size):
         sndata_type, ("sn_ddf_sample",), mb[i], z=zcmb[i]
     )  # can add absmag=-19.9 or other tags
     for j in range(size):
-        covmat[i, j] = y1cov[count]
-        count += 1
+        covmat[i, j] = y1cov[COUNT]
+        COUNT += 1
     for i in range(size):
         covmat[i, i] += (dmb[i]) ** 2
 
@@ -191,6 +190,6 @@ S.metadata["simulation"] = "Y1_DDF_FOUNDATION"
 S.metadata["covmat"] = "sys_Y1_DDF_FOUNDATION_0"
 S.metadata["creation"] = datetime.datetime.now().isoformat()
 S.metadata["info"] = "SN data sets"
-S.save_fits(out_name + ".sacc", overwrite=True)
+S.save_fits(OUT_NAME + ".sacc", overwrite=True)
 # modify this to have interpolation hubble diagrams
 # bias corrections depend on cosmology - systematic vector
