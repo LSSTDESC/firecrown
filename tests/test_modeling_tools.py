d08c4540c230471a445b44b142fd45fb61a1bfcf
Marc Paterno <paterno@fnal.gov>
2023-07-17 13:18:40 -0500

    Do not import np when it is not used

--- tests/test_modeling_tools.py
+++ tests/test_modeling_tools.py
@@ -3,8 +3,8 @@ Tests for the module firecrown.modeling_tools
 """
 import pytest
 import pyccl
-import numpy as np
-from firecrown.modeling_tools import  ModelingTools
+from firecrown.modeling_tools import ModelingTools
+
 
 @pytest.fixture(name="dummy_powerspectrum")
 def make_dummy_powerspectrum() -> pyccl.Pk2D:
@@ -20,10 +20,12 @@ def test_default_constructed_state():
     assert tools.pt_calculator is None
     assert len(tools.powerspectra) == 0
 
+
 def test_default_constructed_no_tools():
     tools = ModelingTools()
     with pytest.raises(RuntimeError):
         _ = tools.get_pk("nonesuch")
+
 
 def test_no_adding_pk_twice(dummy_powerspectrum: pyccl.Pk2D):
     tools = ModelingTools()
