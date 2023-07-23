- Cleaned notebooks. - Fixed cluster_mass_rich_proxy sign error. - Added tests for both numcosmo and scipy integration.

examples/cluster_number_counts/test_integral_abundance.py
@@ -1,25 +1,21 @@
-import os
-import pyccl as ccl
+"""Test integral methods for cluster abundance."""
+
 from typing import Any, Dict
+import time
+
+import pyccl as ccl
 import numpy as np
+from numcosmo_py import Ncm
 
 from firecrown.models.cluster_abundance import ClusterAbundance
 from firecrown.models.cluster_mass_rich_proxy import (
     ClusterMassRich,
     ClusterMassRichBinArgument,
-    ClusterMassRichPointArgument,
 )
 from firecrown.models.cluster_redshift_spec import (
     ClusterRedshiftSpec,
     ClusterRedshiftSpecArgument,
 )
-from firecrown.models.cluster_mass import ClusterMassArgument
-from firecrown.models.cluster_redshift import ClusterRedshiftArgument
-import matplotlib.pyplot as plt
-import scipy
-from scipy.interpolate import interp1d
-import time
-from numcosmo_py import Ncm
 
 Omega_c = 0.262
 Omega_b = 0.049
@@ -51,12 +47,12 @@ pivot_redshift = 0.6
 
 cluster_mass_r = ClusterMassRich(pivot_mass, pivot_redshift)
 
-cluster_mass_r.__setattr__("mu_p0", 3.0)
-cluster_mass_r.__setattr__("mu_p1", 0.86)
-cluster_mass_r.__setattr__("mu_p2", 0.0)
-cluster_mass_r.__setattr__("sigma_p0", 3.0)
-cluster_mass_r.__setattr__("sigma_p1", 0.7)
-cluster_mass_r.__setattr__("sigma_p2", 0.0)
+cluster_mass_r.mu_p0 = 3.0
+cluster_mass_r.mu_p1 = 0.86
+cluster_mass_r.mu_p2 = 0.0
+cluster_mass_r.sigma_p0 = 3.0
+cluster_mass_r.sigma_p1 = 0.7
+cluster_mass_r.sigma_p2 = 0.0
 
 cluster_z = ClusterRedshiftSpec()
 
@@ -97,5 +93,6 @@ for integ_method in integ_options:
     t2 = time.time()
 
     print(
-        f"The time for {integ_method} is {t2-t1}\n\n The counts value is {test_m_list}\n\n"
+        f"The time for {integ_method} is {t2-t1}\n\n"
+        f"The counts value is {test_m_list}\n\n"
     )

firecrown/models/cluster_abundance.py
@@ -9,6 +9,7 @@ from typing import Optional, Any, Dict, List, Tuple, final
 import numpy as np
 import pyccl as ccl
 from numcosmo_py import Ncm
+import scipy.integrate
 
 from ..updatable import Updatable
 from ..parameters import RequiredParameters, DerivedParameterCollection
@@ -56,6 +57,7 @@ class ClusterAbundance(Updatable):
         use_completness: bool = False,
         use_purity: bool = False,
         integ_method: Ncm.IntegralNDMethod = Ncm.IntegralNDMethod.P_V,
+        prefer_scipy_integration: bool = False,
         reltol: float = 1.0e-4,
     ):
         """Initialize the ClusterAbundance class.
@@ -78,6 +80,7 @@ class ClusterAbundance(Updatable):
         self.use_purity = use_purity
         self.integ_method = integ_method
         self.reltol = reltol
+        self.prefer_scipy_integration = prefer_scipy_integration
         if use_completness:
             self.base_mf_d2N_dz_dlnM = self.mf_d2N_dz_dlnM_completeness
         else:
@@ -307,23 +310,37 @@ class ClusterAbundance(Updatable):
                 * redshift_arg.p(arg[0], arg[1])
             )
 
-        Ncm.cfg_init()
-        int_nd = CountsIntegralND(
-            len(index_map), integrand, index_map, arg, ccl_cosmo, mass_arg, redshift_arg
-        )
-        int_nd.set_reltol(self.reltol)
-        res = Ncm.Vector.new(1)
-        err = Ncm.Vector.new(1)
-        int_nd.set_method(self.integ_method)
-        bound_l = []
-        bound_u = []
-        for item in bounds_list:
-            bound_l.append(item[0])
-            bound_u.append(item[1])
-        int_nd.eval(
-            Ncm.Vector.new_array(bound_l), Ncm.Vector.new_array(bound_u), res, err
-        )
-        return res.get(0)
+        if self.prefer_scipy_integration:
+            return scipy.integrate.nquad(
+                integrand,
+                args=(index_map, arg, ccl_cosmo, mass_arg, redshift_arg),
+                ranges=bounds_list,
+                opts={"epsabs": 0.0, "epsrel": self.reltol},
+            )[0]
+        else:
+            Ncm.cfg_init()
+            int_nd = CountsIntegralND(
+                len(index_map),
+                integrand,
+                index_map,
+                arg,
+                ccl_cosmo,
+                mass_arg,
+                redshift_arg,
+            )
+            int_nd.set_reltol(self.reltol)
+            res = Ncm.Vector.new(1)
+            err = Ncm.Vector.new(1)
+            int_nd.set_method(self.integ_method)
+            bound_l = []
+            bound_u = []
+            for item in bounds_list:
+                bound_l.append(item[0])
+                bound_u.append(item[1])
+            int_nd.eval(
+                Ncm.Vector.new_array(bound_l), Ncm.Vector.new_array(bound_u), res, err
+            )
+            return res.get(0)
 
     def compute(
         self,

firecrown/models/cluster_mass_rich_proxy.py
@@ -198,8 +198,8 @@ class ClusterMassRichBinArgument(ClusterMassArgument):
             np.sqrt(2.0) * sigma
         )
 
-        if x_max > 4.0:
+        if x_max > 3.0 or x_min < -3.0:
             #  pylint: disable-next=no-member
-            return (special.erfc(x_min) - special.erfc(x_max)) / 2.0
+            return -(special.erfc(x_min) - special.erfc(x_max)) / 2.0
         #  pylint: disable-next=no-member
         return (special.erf(x_min) - special.erf(x_max)) / 2.0