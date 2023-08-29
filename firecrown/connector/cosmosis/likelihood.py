d1ecccf38a87a150b0165fadbadad3944860e934
Marc Paterno <paterno@fnal.gov>
2023-08-29 18:21:36 -0500

    Remove extraneous import

--- firecrown/connector/cosmosis/likelihood.py
+++ firecrown/connector/cosmosis/likelihood.py
@@ -14,7 +14,6 @@ from cosmosis.datablock import option_section
 from cosmosis.datablock import names as section_names
 import pyccl as ccl
 
-import firecrown.likelihood.gauss_family.gauss_family
 from firecrown.connector.mapping import mapping_builder, MappingCosmoSIS
 from firecrown.likelihood.gauss_family.gauss_family import GaussFamily
 from firecrown.likelihood.gauss_family.statistic.two_point import TwoPoint
