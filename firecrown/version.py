e2cbc68a8287f1f979f1d463d376420dee101ae4
Marc Paterno <paterno@fnal.gov>
2023-10-31 08:57:49 -0500

    Change version number to 1.6.1a0 (#344)

--- firecrown/version.py
+++ firecrown/version.py
@@ -9,5 +9,5 @@ See https://semver.org for details.
 
 FIRECROWN_MAJOR = 1
 FIRECROWN_MINOR = 6
-FIRECROWN_PATCH = "0"
+FIRECROWN_PATCH = "1a0"
 __version__ = f"{FIRECROWN_MAJOR}.{FIRECROWN_MINOR}.{FIRECROWN_PATCH}"
--- tests/test_version.py
+++ tests/test_version.py
@@ -7,4 +7,4 @@ import firecrown
 
 
 def test_version():
-    assert firecrown.__version__ == "1.6.0"
+    assert firecrown.__version__ == "1.6.1a0"
