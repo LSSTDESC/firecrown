11e023e869fb77b255b95688f70b28ecf9a7f3b2
Matt Kwiecien <mattkwiecien@gmail.com>
2023-12-03 00:00:51 -0800

    Improving tests.

--- tests/test_cluster_abundance.py
+++ tests/test_cluster_abundance.py
@@ -52,6 +52,18 @@ def fixture_analytic_sln_solved_kernel():
     return mk
 
 
+@pytest.fixture(name="integrand_args")
+def fixture_integrand_args():
+    sky_area = 439.78986
+    mass = np.linspace(13, 17, 5)
+    z = np.linspace(0, 1, 5)
+    mass_proxy = np.linspace(0, 5, 5)
+    z_proxy = np.linspace(0, 1, 5)
+    mass_proxy_limits = (0, 5)
+    z_proxy_limits = (0, 1)
+    return (mass, z, sky_area, mass_proxy, z_proxy, mass_proxy_limits, z_proxy_limits)
+
+
 def test_cluster_abundance_init(cluster_abundance: ClusterAbundance):
     assert cluster_abundance is not None
     assert cluster_abundance.cosmo is None
@@ -139,127 +151,77 @@ def test_abundance_massfunc_returns_value(cluster_abundance: ClusterAbundance):
 
 @pytest.mark.regression
 def test_abundance_get_integrand(
-    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel
+    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel, integrand_args
 ):
     cosmo = pyccl.CosmologyVanillaLCDM()
     cluster_abundance.update_ingredients(cosmo)
     cluster_abundance.add_kernel(integrable_kernel)
 
-    sky_area = 439.78986
-    mass = np.linspace(13, 17, 5)
-    z = np.linspace(0, 1, 5)
-    mass_proxy = np.linspace(0, 5, 5)
-    z_proxy = np.linspace(0, 1, 5)
-    mass_proxy_limits = (0, 5)
-    z_proxy_limits = (0, 1)
-
     integrand = cluster_abundance.get_integrand()
     assert callable(integrand)
-    result = integrand(
-        mass, z, sky_area, mass_proxy, z_proxy, mass_proxy_limits, z_proxy_limits
-    )
+    result = integrand(*integrand_args)
     assert isinstance(result, np.ndarray)
     assert np.issubdtype(result.dtype, np.float64)
 
 
 @pytest.mark.regression
 def test_abundance_get_integrand_avg_mass(
-    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel
+    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel, integrand_args
 ):
     cosmo = pyccl.CosmologyVanillaLCDM()
     cluster_abundance.update_ingredients(cosmo)
     cluster_abundance.add_kernel(integrable_kernel)
 
-    sky_area = 439.78986
-    mass = np.linspace(13, 17, 5)
-    z = np.linspace(0, 1, 5)
-    mass_proxy = np.linspace(0, 5, 5)
-    z_proxy = np.linspace(0, 1, 5)
-    mass_proxy_limits = (0, 5)
-    z_proxy_limits = (0, 1)
-
     integrand = cluster_abundance.get_integrand(average_properties=ClusterProperty.MASS)
     assert callable(integrand)
-    result = integrand(
-        mass, z, sky_area, mass_proxy, z_proxy, mass_proxy_limits, z_proxy_limits
-    )
+    result = integrand(*integrand_args)
     assert isinstance(result, np.ndarray)
     assert np.issubdtype(result.dtype, np.float64)
 
 
 @pytest.mark.regression
 def test_abundance_get_integrand_avg_redshift(
-    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel
+    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel, integrand_args
 ):
     cosmo = pyccl.CosmologyVanillaLCDM()
     cluster_abundance.update_ingredients(cosmo)
     cluster_abundance.add_kernel(integrable_kernel)
 
-    sky_area = 439.78986
-    mass = np.linspace(13, 17, 5)
-    z = np.linspace(0, 1, 5)
-    mass_proxy = np.linspace(0, 5, 5)
-    z_proxy = np.linspace(0, 1, 5)
-    mass_proxy_limits = (0, 5)
-    z_proxy_limits = (0, 1)
-
     integrand = cluster_abundance.get_integrand(
         average_properties=ClusterProperty.REDSHIFT
     )
     assert callable(integrand)
-    result = integrand(
-        mass, z, sky_area, mass_proxy, z_proxy, mass_proxy_limits, z_proxy_limits
-    )
+    result = integrand(*integrand_args)
     assert isinstance(result, np.ndarray)
     assert np.issubdtype(result.dtype, np.float64)
 
 
 @pytest.mark.regression
 def test_abundance_get_integrand_avg_mass_and_redshift(
-    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel
+    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel, integrand_args
 ):
     cosmo = pyccl.CosmologyVanillaLCDM()
     cluster_abundance.update_ingredients(cosmo)
     cluster_abundance.add_kernel(integrable_kernel)
 
-    sky_area = 439.78986
-    mass = np.linspace(13, 17, 5)
-    z = np.linspace(0, 1, 5)
-    mass_proxy = np.linspace(0, 5, 5)
-    z_proxy = np.linspace(0, 1, 5)
-    mass_proxy_limits = (0, 5)
-    z_proxy_limits = (0, 1)
-
     average_properties = ClusterProperty.MASS | ClusterProperty.REDSHIFT
     integrand = cluster_abundance.get_integrand(average_properties=average_properties)
     assert callable(integrand)
-    result = integrand(
-        mass, z, sky_area, mass_proxy, z_proxy, mass_proxy_limits, z_proxy_limits
-    )
+    result = integrand(*integrand_args)
     assert isinstance(result, np.ndarray)
     assert np.issubdtype(result.dtype, np.float64)
 
 
 @pytest.mark.regression
 def test_abundance_get_integrand_avg_not_implemented_throws(
-    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel
+    cluster_abundance: ClusterAbundance, integrable_kernel: Kernel, integrand_args
 ):
     cosmo = pyccl.CosmologyVanillaLCDM()
     cluster_abundance.update_ingredients(cosmo)
     cluster_abundance.add_kernel(integrable_kernel)
 
-    sky_area = 439.78986
-    mass = np.linspace(13, 17, 5)
-    z = np.linspace(0, 1, 5)
-    mass_proxy = np.linspace(0, 5, 5)
-    z_proxy = np.linspace(0, 1, 5)
-    mass_proxy_limits = (0, 5)
-    z_proxy_limits = (0, 1)
-
     average_properties = ClusterProperty.SHEAR
     integrand = cluster_abundance.get_integrand(average_properties=average_properties)
     assert callable(integrand)
     with pytest.raises(NotImplementedError):
-        _ = integrand(
-            mass, z, sky_area, mass_proxy, z_proxy, mass_proxy_limits, z_proxy_limits
-        )
+        _ = integrand(*integrand_args)
