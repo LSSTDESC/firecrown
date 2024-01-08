0752c3cf0be83e166a94d86d3a381aae6e229ef3
Matt Kwiecien <mattkwiecien@gmail.com>
2024-01-08 13:40:02 -0800

    Removing "generic" logic from integrators.  Keeping integrators simple, they accept a function to integrate and perform that integration.  Adding the ability to pass extra arguments.

--- firecrown/models/cluster/integrator/integrator.py
+++ firecrown/models/cluster/integrator/integrator.py
@@ -3,11 +3,8 @@
 This module holds the classes that define the interface required to
 integrate an assembled cluster abundance.
 """
-import inspect
 from abc import ABC, abstractmethod
-from typing import Tuple, Dict, get_args, List
-from firecrown.models.cluster.abundance import ClusterAbundance, AbundanceIntegrand
-from firecrown.models.cluster.kernel import KernelType
+from typing import Tuple, List, Callable
 
 
 class Integrator(ABC):
@@ -17,48 +14,9 @@ class Integrator(ABC):
     a specific set of methods to be used to integrate a cluster abundance integral."""
 
     def __init__(self) -> None:
-        self.z_proxy_limits: Tuple[float, float] = (-1.0, -1.0)
-        self.mass_proxy_limits: Tuple[float, float] = (-1.0, -1.0)
-        self.sky_area: float = 360**2
         self.integral_bounds: List[Tuple[float, float]] = []
-        self.integral_args_lkp: Dict[KernelType, int] = self._default_integral_args()
+        self.extra_args: List[float] = []
 
     @abstractmethod
-    def integrate(
-        self,
-        integrand: AbundanceIntegrand,
-    ) -> float:
+    def integrate(self, func_to_integrate: Callable) -> float:
         """Call this method to integrate the provided integrand argument."""
-
-    @abstractmethod
-    def set_integration_bounds(
-        self,
-        cl_abundance: ClusterAbundance,
-        sky_area: float,
-        z_proxy_limits: Tuple[float, float],
-        mass_proxy_limits: Tuple[float, float],
-    ) -> None:
-        """Sets the limits of integration used by the integration library.
-
-        This method uses the mass and redshift proxy arguments, along with
-        the cluster abundance argument to construct the limits of integration
-        to be used in evaluating the cluster abundance."""
-
-    def _default_integral_args(self) -> Dict[KernelType, int]:
-        lkp: Dict[KernelType, int] = {}
-        lkp[KernelType.MASS] = 0
-        lkp[KernelType.Z] = 1
-        return lkp
-
-    def _validate_integrand(self, integrand: AbundanceIntegrand) -> None:
-        expected_args, expected_return = get_args(AbundanceIntegrand)
-
-        signature = inspect.signature(integrand)
-        params = signature.parameters.values()
-        param_types = [param.annotation for param in params]
-
-        assert len(params) == len(expected_args)
-
-        assert param_types == list(expected_args)
-
-        assert signature.return_annotation == expected_return
--- firecrown/models/cluster/integrator/scipy_integrator.py
+++ firecrown/models/cluster/integrator/scipy_integrator.py
@@ -2,13 +2,9 @@
 
 This module holds the scipy implementation of the integrator classes
 """
-from typing import Callable, Dict, Tuple
-import numpy as np
-import numpy.typing as npt
+from typing import Callable
 from scipy.integrate import nquad
 from firecrown.models.cluster.integrator.integrator import Integrator
-from firecrown.models.cluster.kernel import KernelType
-from firecrown.models.cluster.abundance import ClusterAbundance, AbundanceIntegrand
 
 
 class ScipyIntegrator(Integrator):
@@ -21,80 +17,14 @@ class ScipyIntegrator(Integrator):
         self._relative_tolerance = relative_tolerance
         self._absolute_tolerance = absolute_tolerance
 
-        self.integral_args_lkp: Dict[KernelType, int] = self._default_integral_args()
-
-    def _integral_wrapper(
-        self,
-        integrand: AbundanceIntegrand,
-    ) -> Callable[..., float]:
-        self._validate_integrand(integrand)
-
-        def scipy_integrand(*int_args: float) -> float:
-            default = -1.0
-            # pylint: disable=R0801
-            mass = self._get_or_default(int_args, KernelType.MASS, default)
-            z = self._get_or_default(int_args, KernelType.Z, default)
-            mass_proxy = self._get_or_default(int_args, KernelType.MASS_PROXY, default)
-            z_proxy = self._get_or_default(int_args, KernelType.Z_PROXY, default)
-
-            return_val = integrand(
-                mass,
-                z,
-                self.sky_area,
-                mass_proxy,
-                z_proxy,
-                self.mass_proxy_limits,
-                self.z_proxy_limits,
-            )[0]
-            assert isinstance(return_val, float)
-            return return_val
-
-        return scipy_integrand
-
-    def set_integration_bounds(
-        self,
-        cl_abundance: ClusterAbundance,
-        sky_area: float,
-        z_proxy_limits: Tuple[float, float],
-        mass_proxy_limits: Tuple[float, float],
-    ) -> None:
-        # pylint: disable=R0801
-        self.integral_args_lkp = self._default_integral_args()
-        self.integral_bounds = [
-            (cl_abundance.min_mass, cl_abundance.max_mass),
-            (cl_abundance.min_z, cl_abundance.max_z),
-        ]
-
-        self.mass_proxy_limits = mass_proxy_limits
-        self.z_proxy_limits = z_proxy_limits
-        self.sky_area = sky_area
-
-        for kernel in cl_abundance.dirac_delta_kernels:
-            if kernel.kernel_type == KernelType.Z_PROXY:
-                self.integral_bounds[1] = z_proxy_limits
-
-            elif kernel.kernel_type == KernelType.MASS_PROXY:
-                self.integral_bounds[0] = mass_proxy_limits
-
-        for kernel in cl_abundance.integrable_kernels:
-            idx = len(self.integral_bounds)
-
-            if kernel.kernel_type == KernelType.Z_PROXY:
-                self.integral_bounds.append(z_proxy_limits)
-                self.integral_args_lkp[KernelType.Z_PROXY] = idx
-
-            elif kernel.kernel_type == KernelType.MASS_PROXY:
-                self.integral_bounds.append(mass_proxy_limits)
-                self.integral_args_lkp[KernelType.MASS_PROXY] = idx
-
     def integrate(
         self,
-        integrand: AbundanceIntegrand,
+        func_to_integrate: Callable,
     ) -> float:
-        scipy_integrand = self._integral_wrapper(integrand)
         val = nquad(
-            scipy_integrand,
+            func_to_integrate,
             ranges=self.integral_bounds,
+            args=self.extra_args,
             opts={
                 "epsabs": self._absolute_tolerance,
                 "epsrel": self._relative_tolerance,
@@ -102,14 +32,3 @@ class ScipyIntegrator(Integrator):
         )[0]
         assert isinstance(val, float)
         return val
-
-    def _get_or_default(
-        self,
-        int_args: Tuple[float, ...],
-        kernel_type: KernelType,
-        default: float,
-    ) -> npt.NDArray[np.float64]:
-        try:
-            return np.atleast_1d(int_args[self.integral_args_lkp[kernel_type]])
-        except KeyError:
-            return np.atleast_1d(default)
