e5372caa3be938f38d1aeeec356cd6b574ee440e
Matt Kwiecien <mattkwiecien@gmail.com>
2024-01-08 14:10:54 -0800

    Updating the likelihood script to just create the new recipe.

--- examples/cluster_number_counts/cluster_redshift_richness.py
+++ examples/cluster_number_counts/cluster_redshift_richness.py
@@ -1,24 +1,22 @@
 """Likelihood factory function for cluster number counts."""
 
 import os
+from typing import Tuple
 
 import pyccl as ccl
 import sacc
 
-from firecrown.models.cluster.integrator.numcosmo_integrator import NumCosmoIntegrator
 from firecrown.likelihood.gauss_family.gaussian import ConstGaussian
 from firecrown.likelihood.gauss_family.statistic.binned_cluster_number_counts import (
     BinnedClusterNumberCounts,
 )
-from firecrown.models.cluster.properties import ClusterProperty
+from firecrown.likelihood.likelihood import Likelihood, NamedParameters
 from firecrown.modeling_tools import ModelingTools
 from firecrown.models.cluster.abundance import ClusterAbundance
-from firecrown.models.cluster.kernel import (
-    SpectroscopicRedshift,
+from firecrown.models.cluster.properties import ClusterProperty
+from firecrown.models.cluster.recipes.murata_binned_spec_z import (
+    MurataBinnedSpecZRecipe,
 )
-from firecrown.models.cluster.mass_proxy import MurataBinned
-from firecrown.likelihood.likelihood import NamedParameters, Likelihood
-from typing import Tuple
 
 
 def get_cluster_abundance() -> ClusterAbundance:
@@ -26,14 +24,6 @@ def get_cluster_abundance() -> ClusterAbundance:
     min_mass, max_mass = 13.0, 16.0
     min_z, max_z = 0.2, 0.8
     cluster_abundance = ClusterAbundance(min_mass, max_mass, min_z, max_z, hmf)
-
-    # Create and add the kernels you want in your cluster abundance
-    pivot_mass, pivot_redshift = 14.625862906, 0.6
-    mass_observable_kernel = MurataBinned(pivot_mass, pivot_redshift)
-    cluster_abundance.add_kernel(mass_observable_kernel)
-
-    redshift_proxy_kernel = SpectroscopicRedshift()
-    cluster_abundance.add_kernel(redshift_proxy_kernel)
 
     return cluster_abundance
 
@@ -44,19 +34,17 @@ def build_likelihood(
     """
     Here we instantiate the number density (or mass function) object.
     """
-    integrator = NumCosmoIntegrator()
-    # integrator = ScipyIntegrator()
 
     # Pull params for the likelihood from build_parameters
-    average_properties = ClusterProperty.NONE
+    average_on = ClusterProperty.NONE
     if build_parameters.get_bool("use_cluster_counts", True):
-        average_properties |= ClusterProperty.COUNTS
+        average_on |= ClusterProperty.COUNTS
     if build_parameters.get_bool("use_mean_log_mass", False):
-        average_properties |= ClusterProperty.MASS
+        average_on |= ClusterProperty.MASS
 
     survey_name = "numcosmo_simulated_redshift_richness"
     likelihood = ConstGaussian(
-        [BinnedClusterNumberCounts(average_properties, survey_name, integrator)]
+        [BinnedClusterNumberCounts(average_on, survey_name, MurataBinnedSpecZRecipe())]
     )
 
     # Read in sacc data
