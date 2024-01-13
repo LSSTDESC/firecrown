86be99df90a31e1ac363ba9d148f6f18e214d366
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2024-01-13 15:36:57 -0300

    Removing more redundancies.

--- firecrown/likelihood/gauss_family/gaussian.py
+++ firecrown/likelihood/gauss_family/gaussian.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 import numpy as np
 
-from .gauss_family import GaussFamily, State
+from .gauss_family import GaussFamily
 from ...modeling_tools import ModelingTools
 
 
@@ -18,16 +18,6 @@ class ConstGaussian(GaussFamily):
         return -0.5 * self.compute_chisq(tools)
 
     def make_realization_vector(self) -> np.ndarray:
-        assert (
-            self.state == State.UPDATED
-        ), "update() must be called before make_realization_vector()"
-
-        if not self.computed_theory_vector:
-            raise RuntimeError(
-                "The theory vector has not been computed yet. "
-                "Call compute_theory_vector first."
-            )
-
         theory_vector = self.get_theory_vector()
         assert self.cholesky is not None
         new_data_vector = theory_vector + np.dot(
