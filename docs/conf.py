0dcf2c6184568c0acfaad50bfe394b308f18c3ab
Matt Kwiecien <mattkwiecien@gmail.com>
2024-02-20 10:56:15 -0800

    Incrementing firecrown version number

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022--2024, LSST DESC Firecrown Contributors"
 author = "LSST DESC Firecrown Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "1.7.0"
+release = "1.7.1"
 
 
 # -- General configuration ---------------------------------------------------
