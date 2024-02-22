a177860b8a139a541fec7107d47dc14cf4c3d926
Matt Kwiecien <mattkwiecien@gmail.com>
2024-02-21 16:44:32 -0800

    Trying to include downloads links into the new theme.

--- docs/conf.py
+++ docs/conf.py
@@ -72,6 +72,12 @@ html_title = "firecrown"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+downloads = [
+    ("pdf", "//firecrown.readthedocs.io/_/downloads/en/latest/pdf/"),
+    ("html", "//firecrown.readthedocs.io/_/downloads/en/latest/htmlzip/"),
+    ("epub", "//firecrown.readthedocs.io/_/downloads/en/latest/epub/"),
+]
+
 
 # -- Extension configuration -------------------------------------------------
 
