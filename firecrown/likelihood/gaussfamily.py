Fixing covariance size test.

firecrown/likelihood/gaussfamily.py
@@ -243,10 +243,17 @@ class GaussFamily(Likelihood):
         data_vector = np.concatenate(data_vector_list)
         cov = np.zeros((len(indices), len(indices)))
 
-        if covariance.shape != (len(indices), len(indices)):
+        largest_index = np.max(indices)
+
+        if not (
+            covariance.ndim == 2
+            and covariance.shape[0] == covariance.shape[1]
+            and largest_index < covariance.shape[0]
+        ):
             raise ValueError(
                 f"The covariance matrix has shape {covariance.shape}, "
-                f"but the expected shape is {(len(indices), len(indices))}."
+                f"but the expected shape is at least "
+                f"{(largest_index+1, largest_index+1)}."
             )
 
         for new_i, old_i in enumerate(indices):

tests/likelihood/gauss_family/test_const_gaussian.py
@@ -508,7 +508,7 @@ def test_create_ready_wrong_size(sacc_galaxy_cwindows):
         ValueError,
         match=re.escape(
             f"The covariance matrix has shape (3, 3), "
-            f"but the expected shape is ({size}, {size})."
+            f"but the expected shape is at least ({size}, {size})."
         ),
     ):
         ConstGaussian.create_ready(two_points, np.diag([1.0, 2.0, 3.0]))