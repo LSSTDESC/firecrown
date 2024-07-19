aeab23b5b77ecff70f4df88c4cd162d78aed4290
Yuanyuan Zhang <ynzhang@umich.edu>
2024-07-19 15:04:09 +0200

    fomrmatting the DS_from_mass class and test

--- firecrown/models/cluster/DS_from_mass.py
+++ firecrown/models/cluster/DS_from_mass.py
@@ -1,13 +1,12 @@
-"""The module responsible for building a Delta_sigma calculation from mass and radius."""
+"""The module builds a Delta_sigma calculation from mass, redshift and radius."""
 
 import os
 import numpy as np
 import numpy.typing as npt
-import pyccl
-import pyccl.background as bkg
 from pyccl.cosmology import Cosmology
 from firecrown.updatable import Updatable, UpdatableCollection
 import clmm
+
 
 class DS_from_mass(Updatable):
     """The class that calculates the predicted number counts of galaxy clusters.
@@ -29,36 +28,45 @@ class DS_from_mass(Updatable):
         super().__init__()
         self.kernels: UpdatableCollection = UpdatableCollection()
         self._cosmo: Cosmology = None
-        
+
         # Setting things up for CLMM
-        os.environ['CLMM_MODELING_BACKEND'] = 'ccl'
+        os.environ["CLMM_MODELING_BACKEND"] = "ccl"
         self.cosmo_clmm = clmm.Cosmology()
-        self.moo = clmm.Modeling(massdef="mean", delta_mdef=200, halo_profile_model="nfw")
+        self.moo = clmm.Modeling(
+            massdef="mean", delta_mdef=200, halo_profile_model="nfw"
+        )
 
     def update_ingredients(self, cosmo: Cosmology) -> None:
         """Update the calculation with a new cosmology."""
         self._cosmo = cosmo
-         
+
         # Setting things up for CLMM
         self.cosmo_clmm._init_from_cosmo(cosmo)
         self.moo.set_cosmo(self.cosmo_clmm)
-        # assuming the same concentration for all masses. 
+        # assuming the same concentration for all masses.
         # Not realistic, but avoid having to call a mass-concentration relation.
-        self.moo.set_concentration(4) 
+        self.moo.set_concentration(4)
 
     def calculate_DS_from_Mass(
-        self, 
-        log10_mass:npt.NDArray[np.float64],
-        redshift:npt.NDArray[np.float64],
-        radii:npt.NDArray[np.float64],
-        ) -> npt.NDArray[np.float64]:
-        
-      
+        self,
+        log10_mass: npt.NDArray[np.float64],
+        redshift: npt.NDArray[np.float64],
+        radii: npt.NDArray[np.float64],
+    ) -> npt.NDArray[np.float64]:
+        """
+        Calculate DeltaSigma for a given log10_mass, redshift, and radii.
+
+        Input:
+        log10_mass: numpy.NDArray[np.float64]; cluster mass value in log10.
+        redshift: numpy.NDArray[np.float64]; redshift values.
+        radii: numpy.NDArray[np.float64]; radius to compute DeltaSigma at.
+
+        Output:
+        profile: numpy NDArray[np.float64]; delta_sigma profile corresponding to
+        the log10_mass, redshift and radii
+        """
         mass = 10**log10_mass
         self.moo.set_mass(mass)
         profile = self.moo.eval_excess_surface_density(radii, redshift)
-         
-        print("check:", profile)
-        #assert isinstance(profile, np.ndarray)
-        return profile
 
+        return profile
--- tests/test_cluster_DSMass.py
+++ tests/test_cluster_DSMass.py
@@ -9,28 +9,26 @@ from firecrown.models.cluster.DS_from_mass import DS_from_mass
 
 @pytest.fixture(name="ds_from_mass")
 def fixture_ds_from_mass():
-    """Test fixture that represents an assembled cluster abundance class."""
+    """Test fixture that represents a  cluster DS_from_mass class."""
     ds = DS_from_mass()
     return ds
 
+
 def test_ds_from_mass_init(ds_from_mass: DS_from_mass):
+    """Test the initiation of a cluster DS_from_mass class."""
     assert ds_from_mass is not None
     assert ds_from_mass.cosmo is None
-    assert isinstance(
-           ds_from_mass.moo, clmm.Modeling
-    )
-    assert isinstance(
-           ds_from_mass.cosmo_clmm, clmm.Cosmology
-    )
-    assert isinstance(
-           ds_from_mass.moo, clmm.Modeling
-    )
+    assert isinstance(ds_from_mass.moo, clmm.Modeling)
+    assert isinstance(ds_from_mass.cosmo_clmm, clmm.Cosmology)
+    assert isinstance(ds_from_mass.moo, clmm.Modeling)
+
 
 def test_ds_from_mass_update_ingredients(ds_from_mass: DS_from_mass):
+    """Test the update of a cluster DS_from_mass class."""
     cosmo = pyccl.CosmologyVanillaLCDM()
-    clmm_cosmo=clmm.Cosmology()
+    clmm_cosmo = clmm.Cosmology()
     clmm_cosmo._init_from_cosmo(cosmo)
-    
+
     ds_from_mass.update_ingredients(cosmo)
     assert ds_from_mass.cosmo is not None
     assert ds_from_mass.cosmo == cosmo
@@ -38,26 +36,22 @@ def test_ds_from_mass_update_ingredients(ds_from_mass: DS_from_mass):
 
 
 def test_ds_from_mass_calculate_DS_from_Mass(ds_from_mass: DS_from_mass):
-    
+    """Test the profile calculation of a cluster DS_from_mass class."""
     cosmo = pyccl.CosmologyVanillaLCDM()
-    clmm_cosmo=clmm.Cosmology()
+    clmm_cosmo = clmm.Cosmology()
     clmm_cosmo._init_from_cosmo(cosmo)
     moo = clmm.Modeling(massdef="mean", delta_mdef=200, halo_profile_model="nfw")
     moo.set_cosmo(clmm_cosmo)
     moo.set_concentration(4)
-    mass=14.0
-    redshift =1.0
+    mass = 14.0
+    redshift = 1.0
     moo.set_mass(mass)
-    radii=np.arange(0.1, 1.0, 0.1)
+    radii = np.arange(0.1, 1.0, 0.1)
     profile = moo.eval_excess_surface_density(radii, redshift)
 
     ds_from_mass.update_ingredients(cosmo)
-    
-    assert ds_from_mass.calculate_DS_from_Mass(mass, redshift, radii).all() == profile.all() 
 
-
-
-
-
-
-
+    assert (
+        ds_from_mass.calculate_DS_from_Mass(mass, redshift, radii).all()
+        == profile.all()
+    )
