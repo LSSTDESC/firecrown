2dc3cd2f45871aabf98ed0c957220ba6ff4a6fd3
Marc Paterno <paterno@fnal.gov>
2024-12-12 18:07:25 -0600

    Initial version of tracing code

--- tests/test_number_counts_magnification_bias_systematic.py
+++ tests/test_number_counts_magnification_bias_systematic.py
@@ -23,6 +23,54 @@ def test_creation_nc_magnification_bias_systematic():
 def test_update_nc_magnification_bias_systematic(
     tools_with_vanilla_cosmology: mt.ModelingTools,
 ):
+    from sys import settrace
+
+    # some global context to be used in the tracing. We are relying on
+    # 'trace_call' to act as a closure that captures these names.
+    f = open("trace.log", "w")  # the file used for logging
+    level = 0  # the call nesting level
+    entry = 0  # sequential entry number for each record
+
+    print("entry\tevent\tlevel\tfunction\tvalue\textra", file=f)
+
+    def trace_call(fr, ev, arg):
+        nonlocal level
+        nonlocal entry
+        code = fr.f_code
+        extra = ""
+        match ev:
+            case "call":
+                entry += 1
+                level += 1
+                nargs = code.co_argcount
+                # slice the tuple to get only argument names
+                argnames = code.co_varnames[:nargs]
+                if nargs > 0 and code.co_varnames[0] == "self":
+                    val = fr.f_locals["self"]
+                    extra = f"{type(val).__name__}"
+                print(
+                    f"{entry}\tcall\t{level}\t{code.co_qualname}\t{argnames}\t{extra}",
+                    file=f,
+                )
+            case "return":
+                entry += 1
+                extra = f"{type(arg).__name__}"
+                print(
+                    f"{entry}\treturn\t{level}\t{code.co_qualname}\t{arg}\t{extra}",
+                    file=f,
+                )
+                level -= 1
+            case "exception":
+                entry += 1
+                print(
+                    f"{entry}\texception\t{level}\t{code.co_qualname}\t\t{extra}",
+                    file=f,
+                )
+        return trace_call
+
+    settrace(trace_call)
+
+    # Now we have the read test...
     sys = nc.MagnificationBiasSystematic(sacc_tracer="lens0")
     assert sys.parameter_prefix == "lens0"
     sys.update(
@@ -56,3 +104,5 @@ def test_update_nc_magnification_bias_systematic(
 
     assert np.allclose(new_z, expected_zs)
     assert np.allclose(new_mag_bias, expected_mag_biases)
+    settrace(None)
+    f.close()
