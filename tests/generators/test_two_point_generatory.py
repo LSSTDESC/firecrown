2a05e952952e23ea5b3e42cb5d6970629021574b
Marc Paterno <paterno@fnal.gov>
2025-01-14 08:36:03 -0600

    Add test for apply_theta_min_max

--- tests/generators/test_two_point_generatory.py
+++ tests/generators/test_two_point_generatory.py
@@ -1,17 +1,14 @@
 """Tests for the firecrown.generators.two_point module."""
 
-from firecrown.generators import two_point
-
-
 import numpy as np
 import numpy.typing as npt
 import pytest
 
-from firecrown.generators.two_point import apply_ells_min_max
+from firecrown.generators.two_point import apply_ells_min_max, apply_theta_min_max
 
 
 @pytest.mark.parametrize(
-    "ells, Cells, indices, ell_min, ell_max, expected_ells, expected_Cells, expected_indices",
+    "ells, Cells, indices, ell_min, ell_max, res_ells, res_Cells, res_indices",
     [
         (
             np.array([2, 3, 4, 5, 6]),
@@ -61,9 +58,9 @@ def test_apply_ells_min_max(
     indices: None | npt.NDArray[np.int64],
     ell_min: None | int,
     ell_max: None | int,
-    expected_ells: np.typing.NDArray[np.int64],
-    expected_Cells: np.typing.NDArray[np.float64],
-    expected_indices: None | np.typing.NDArray[np.int64],
+    res_ells: np.typing.NDArray[np.int64],
+    res_Cells: np.typing.NDArray[np.float64],
+    res_indices: None | np.typing.NDArray[np.int64],
 ):
     """Test the apply_ells_min_max function."""
     (
@@ -71,6 +68,80 @@ def test_apply_ells_min_max(
         result_Cells,
         result_indices,
     ) = apply_ells_min_max(ells, Cells, indices, ell_min, ell_max)
-    np.testing.assert_array_equal(result_ells, expected_ells)
-    np.testing.assert_array_equal(result_Cells, expected_Cells)
-    np.testing.assert_array_equal(result_indices, expected_indices)
+    np.testing.assert_array_equal(result_ells, res_ells)
+    np.testing.assert_array_equal(result_Cells, res_Cells)
+    if indices is None:
+        assert result_indices is None
+    else:
+        assert result_indices is not None
+        assert res_indices is not None
+        np.testing.assert_array_equal(result_indices, res_indices)
+
+
+@pytest.mark.parametrize(
+    "thetas, xis, indices, theta_min, theta_max, res_thetas, res_xis, res_indices",
+    [
+        (
+            np.array([10.0, 20.0, 30.0, 40.0, 50.0]),
+            np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
+            np.array([0, 1, 2, 3, 4]),
+            None,
+            None,
+            np.array([10.0, 20.0, 30.0, 40.0, 50.0]),
+            np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
+            np.array([0, 1, 2, 3, 4]),
+        ),
+        (
+            np.array([10.0, 20.0, 30.0, 40.0, 50.0]),
+            np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
+            np.array([0, 1, 2, 3, 4]),
+            20.0,
+            None,
+            np.array([20.0, 30.0, 40.0, 50.0]),
+            np.array([2.0, 3.0, 4.0, 5.0]),
+            np.array([1, 2, 3, 4]),
+        ),
+        (
+            np.array([10.0, 20.0, 30.0, 40.0, 50.0]),
+            np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
+            np.array([0, 1, 2, 3, 4]),
+            None,
+            40.0,
+            np.array([10.0, 20.0, 30.0, 40.0]),
+            np.array([1.0, 2.0, 3.0, 4.0]),
+            np.array([0, 1, 2, 3]),
+        ),
+        (
+            np.array([10.0, 20.0, 30.0, 40.0, 50.0]),
+            np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
+            None,
+            20.0,
+            40.0,
+            np.array([20.0, 30.0, 40.0]),
+            np.array([2.0, 3.0, 4.0]),
+            None,
+        ),
+    ],
+)
+def test_apply_theta_min_max(
+    thetas: npt.NDArray[np.float64],
+    xis: npt.NDArray[np.float64],
+    indices: None | npt.NDArray[np.int64],
+    theta_min: None | float,
+    theta_max: None | float,
+    res_thetas: npt.NDArray[np.float64],
+    res_xis: npt.NDArray[np.float64],
+    res_indices: None | npt.NDArray[np.int64],
+):
+    result_thetas, result_xis, result_indices = apply_theta_min_max(
+        thetas, xis, indices, theta_min, theta_max
+    )
+
+    np.testing.assert_array_equal(result_thetas, res_thetas)
+    np.testing.assert_array_equal(result_xis, res_xis)
+    if indices is not None:
+        assert result_indices is not None
+        assert res_indices is not None
+        np.testing.assert_array_equal(result_indices, res_indices)
+    else:
+        assert result_indices is None
