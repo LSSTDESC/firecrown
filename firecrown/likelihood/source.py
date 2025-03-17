33c97f2ba5dbd17407e7002af57418f9cd31c4aa
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-03-17 17:50:37 -0300

    Adding padding values to avoid extrapolation at the edges due to roundoff.

--- firecrown/likelihood/source.py
+++ firecrown/likelihood/source.py
@@ -349,7 +349,11 @@ def dndz_shift_and_stretch_active(
     if sigma_z <= 0.0:
         raise ValueError("Stretch Parameter must be positive")
 
-    dndz_interp = Akima1DInterpolator(z, dndz, method="makima")
+    # We need a small padding to avoid extrapolation at the edges
+    padding = 1.0e-8
+    z_padded = np.concatenate([[z[0] * (1.0 - padding)], z, [z[-1] * (1.0 + padding)]])
+    dndz_padded = np.concatenate([[dndz[0]], dndz, [dndz[-1]]])
+    dndz_interp = Akima1DInterpolator(z_padded, dndz_padded, method="makima")
     dndz_mean = np.average(z, weights=dndz)
 
     z_new = (z - dndz_mean + delta_z) / sigma_z + dndz_mean
