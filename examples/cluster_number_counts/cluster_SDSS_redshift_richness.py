de6ba1b5f44f9b4fc515767ba884c58d62f2a184
Yuanyuan Zhang <ynzhang@umich.edu>
2025-03-24 11:27:10 -0700

    Cluster example implementing SDSS cluster number counts and mean masses.  (#443)
    * updated syntax in demo_sacc_for_clusters.ipynp to match the syntax used in generate_rich_mean_mass_sacc_data.py
    * adding a new file to generate SACC file with SDSS cluster data vector
    * running black and flake8 to reformat two modified pieces of code
    * changing lambda bins to log(lambda) bins
    * adding a configuration of the pipeline to run for the SDSS data vector (N+M)
    * Cleaning notebooks.
    * Updated ClusterAbundance usage.
    * Fixed type hint.
    * Updated tests to check new example.
    * changing the max_mass from 16.0 to 17.0 to make it work with a larger parameter space. Max_mass of 16.0 gives frequent errors when trying to run the chain
    
    ---------
    
    Co-authored-by: Sandro Dias Pinto Vitenti <vitenti@uel.br>

---
+++ examples/cluster_number_counts/cluster_SDSS_redshift_richness.py
@@ -0,0 +1,58 @@
+"""Likelihood factory function for cluster number counts."""
+
+import os
+
+import pyccl as ccl
+import sacc
+
+from firecrown.likelihood.gaussian import ConstGaussian
+from firecrown.likelihood.binned_cluster_number_counts import (
+    BinnedClusterNumberCounts,
+)
+from firecrown.likelihood.likelihood import Likelihood, NamedParameters
+from firecrown.modeling_tools import ModelingTools
+from firecrown.models.cluster.abundance import ClusterAbundance
+from firecrown.models.cluster.properties import ClusterProperty
+from firecrown.models.cluster.recipes.murata_binned_spec_z import (
+    MurataBinnedSpecZRecipe,
+)
+
+
+def get_cluster_abundance() -> ClusterAbundance:
+    """Creates and returns a ClusterAbundance object."""
+    hmf = ccl.halos.MassFuncBocquet16()
+    min_mass, max_mass = 13.0, 17.0
+    min_z, max_z = 0.2, 0.8
+    cluster_abundance = ClusterAbundance((min_mass, max_mass), (min_z, max_z), hmf)
+
+    return cluster_abundance
+
+
+def build_likelihood(
+    build_parameters: NamedParameters,
+) -> tuple[Likelihood, ModelingTools]:
+    """Builds the likelihood for Firecrown."""
+    # Pull params for the likelihood from build_parameters
+    average_on = ClusterProperty.NONE
+    if build_parameters.get_bool("use_cluster_counts", True):
+        average_on |= ClusterProperty.COUNTS
+    if build_parameters.get_bool("use_mean_log_mass", False):
+        average_on |= ClusterProperty.MASS
+
+    survey_name = "SDSSCluster_redshift_richness"
+    likelihood = ConstGaussian(
+        [BinnedClusterNumberCounts(average_on, survey_name, MurataBinnedSpecZRecipe())]
+    )
+
+    # Read in sacc data
+    sacc_file_nm = "SDSSTEST_redshift_richness_sacc_data.fits"
+    sacc_path = os.path.expanduser(
+        os.path.expandvars("${FIRECROWN_DIR}/examples/cluster_number_counts/")
+    )
+    sacc_data = sacc.Sacc.load_fits(os.path.join(sacc_path, sacc_file_nm))
+    likelihood.read(sacc_data)
+
+    cluster_abundance = get_cluster_abundance()
+    modeling_tools = ModelingTools(cluster_abundance=cluster_abundance)
+
+    return likelihood, modeling_tools
--- tests/integration/test_cluster_number_counts.py
+++ tests/integration/test_cluster_number_counts.py
@@ -29,6 +29,28 @@ def test_cluster_number_counts_cosmosis():
 
 
 @pytest.mark.integration
+def test_cluster_number_counts_sdss_cosmosis():
+    result = subprocess.run(
+        [
+            "bash",
+            "-c",
+            """
+                set -e
+                cd examples/cluster_number_counts
+                python generate_SDSS_ClusterCountsMass_sacc_data.py
+                cosmosis cluster_SDSS_counts_mean_mass_redshift_richness.ini
+            """,
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+
+    assert result.returncode == 0
+    print(result.stdout)
+
+
+@pytest.mark.integration
 def test_cluster_number_counts_numcosmo():
     result = subprocess.run(
         [
@@ -46,6 +68,10 @@ def test_cluster_number_counts_numcosmo():
                 numcosmo from-cosmosis cluster_counts_mean_mass_redshift_richness.ini\\
                     --matter-ps eisenstein_hu
                 numcosmo run test cluster_counts_mean_mass_redshift_richness.yaml
+                numcosmo from-cosmosis \\
+                    cluster_SDSS_counts_mean_mass_redshift_richness.ini \\
+                    --matter-ps eisenstein_hu
+                numcosmo run test cluster_SDSS_counts_mean_mass_redshift_richness.yaml
             """,
         ],
         capture_output=True,
