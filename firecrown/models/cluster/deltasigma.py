d4509d221129b31d7c20497139bf6afdf227f2e3
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-05-13 16:37:37 +0200

    Initial Implementation

--- firecrown/models/cluster/deltasigma.py
+++ firecrown/models/cluster/deltasigma.py
@@ -8,6 +8,8 @@ functions that produce those predictions.
 import numpy as np
 import numpy.typing as npt
 import pyccl
+from scipy.stats import gamma
+from firecrown.models.cluster.integrator.numcosmo_integrator import NumCosmoIntegrator
 
 import clmm  # pylint: disable=import-error
 from firecrown import parameters
@@ -41,7 +43,10 @@ class ClusterDeltaSigma(ClusterAbundance):
         self,
         log_mass: npt.NDArray[np.float64],
         z: npt.NDArray[np.float64],
-        radius_center: float,
+        radius_center: np.float64,
+        two_halo_term: bool = False,
+        miscentering_frac: np.float64 = None,
+        boost_factor: bool = False,
     ) -> npt.NDArray[np.float64]:
         """Delta sigma for clusters."""
         cosmo_clmm = clmm.Cosmology()
@@ -68,7 +73,9 @@ class ClusterDeltaSigma(ClusterAbundance):
                 conc_val = conc._concentration(self._cosmo, 10**log_m, a)
                 moo.set_concentration(conc_val)
                 moo.set_mass(10**log_m)
-                val = moo.eval_excess_surface_density(radius_center, redshift)
+                val = self._one_halo_contribution(moo, radius_center, redshift, miscentering_frac)
+                if two_halo_term:
+                    val+= self._two_halo_contribution(moo, radius_center, redshift)
                 return_vals.append(val)
         else:
             conc_val = self.cluster_conc
@@ -76,6 +83,40 @@ class ClusterDeltaSigma(ClusterAbundance):
             for log_m, redshift in zip(log_mass, z):
                 moo.set_concentration(conc_val)
                 moo.set_mass(10**log_m)
-                val = moo.eval_excess_surface_density(radius_center, redshift)
+                val = self._one_halo_contribution(moo, radius_center, redshift, miscentering_frac)
+                if two_halo_term:
+                    val+= self._two_halo_contribution(moo, radius_center, redshift)
                 return_vals.append(val)
+        if boost_factor:
+            boost_factor_val = clmm.utils.compute_nfw_boost(radius_center, r_scale = 1.0)
+            return_vals = [val / boost_factor_val for val in return_vals]
         return np.asarray(return_vals, dtype=np.float64)
+
+
+    def _one_halo_contribution(self, clmm_model: clmm.Modeling, radius_center, redshift, miscentering_frac = None) -> npt.NDArray[np.float64]:
+        """Calculate the second halo contribution to the delta sigma."""
+        # pylint: disable=protected-access
+        first_halo_right_centered = clmm_model.eval_excess_surface_density(radius_center, redshift)
+        if miscentering_frac is not None:
+            integrator = NumCosmoIntegrator()
+            def integration_func(int_args, extra_args):
+                sigma = extra_args[0]
+                r_mis_list = int_args[:, 0]
+                esd_vals = np.array([
+                    clmm_model.eval_excess_surface_density(np.array([radius_center]), redshift, r_mis=r_mis)[0]
+                    for r_mis in r_mis_list
+                ])
+                gamma_vals = gamma.pdf(r_mis_list, a=2.0, scale=sigma)
+                return esd_vals * gamma_vals
+            integrator.integral_bounds = [(0.0, 2.0)]
+            integrator.extra_args = np.array([0.12])  ## From https://arxiv.org/pdf/2502.08444
+            miscentering_integral = integrator.integrate(integration_func)
+            return (1.0 - miscentering_frac) * first_halo_right_centered + miscentering_frac * miscentering_integral
+        return first_halo_right_centered[0]
+
+    def _two_halo_contribution(self, clmm_model: clmm.Modeling, radius_center, redshift) -> npt.NDArray[np.float64]:
+        """Calculate the second halo contribution to the delta sigma."""
+        # pylint: disable=protected-access
+        second_halo_right_centered = clmm_model.eval_excess_surface_density_2h(np.array([radius_center]), redshift)
+        return second_halo_right_centered[0]
+
