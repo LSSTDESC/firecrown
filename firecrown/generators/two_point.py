94d342463e9d28ad0951b8ac9d2779b9cdec1112
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-05-28 11:40:13 -0300

    Simplify multipole interp (#522)
    * Simplify configuration of multipole orders for interpolation.
    * Fixing minimum ell computation.
    
    ---------
    
    Co-authored-by: Marc Paterno <paterno@fnal.gov>

--- firecrown/generators/two_point.py
+++ firecrown/generators/two_point.py
@@ -2,32 +2,38 @@
 
 from __future__ import annotations
 
-import copy
 from typing import Annotated, TypedDict
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, ConfigDict
 import numpy as np
 import numpy.typing as npt
 
 
-ELL_FOR_XI_DEFAULTS = {"minimum": 2, "midpoint": 50, "maximum": 60_000, "n_log": 200}
-
-
 class LogLinearElls(BaseModel):
     """Generator for log-linear integral ell values.
+
+    The initializer for LogLinearElls accepts only named parameters. A default
+    value is provided for each parameter, which default is used if the parameter
+    is not provided to the initializer.
 
     Not all ell values will be generated. The result will contain each integral
     value from min to mid. Starting from mid, and going up to max, there will be
     n_log logarithmically spaced values.
 
     Note that midpoint must be strictly greater than minimum, and strictly less
-    than maximum. n_log must be positive.
+    than maximum. n_log must be positive. All must be integers. All these conditions
+    are verified in the initializer.
+
+    LogLinearElls objects are immutable, so they are safe to share and to use as
+    default values for parameters.
     """
 
-    minimum: Annotated[int, Field(ge=0)]
-    midpoint: Annotated[int, Field(ge=0)]
-    maximum: Annotated[int, Field(ge=0)]
-    n_log: Annotated[int, Field(ge=1)]
+    model_config = ConfigDict(extra="forbid", frozen=True)
+
+    minimum: Annotated[int, Field(ge=0)] = 2
+    midpoint: Annotated[int, Field(ge=0)] = 50
+    maximum: Annotated[int, Field(ge=0)] = 60_000
+    n_log: Annotated[int, Field(ge=1)] = 200
 
     @model_validator(mode="after")
     def require_increasing(self) -> "LogLinearElls":
@@ -36,51 +42,33 @@ class LogLinearElls(BaseModel):
         assert self.midpoint < self.maximum
         return self
 
-    def generate(self) -> npt.NDArray[np.int64]:
+    def generate(
+        self, min_ell: int | None = None, max_ell: int | None = None
+    ) -> npt.NDArray[np.int64]:
         """Generate the log-linear ell values.
 
-        The result will contain each integral value from min to mid. Starting
-        from mid, and going up to max, there will be n_log logarithmically
-        spaced values.
+        This will use the object's minimum, midpoint, maximum, and n_log,
+        unless min_ell and max_ell are provided, in which case those will be used.
 
-        :param minimum: The low edge of the first bin.
-        :param midpoint: The high edge of the last in the linear range.
-        :param maximum: The high edge of the last bin.
-        :param n_log: The number of bins in the log section of the range.
+        :param min_ell: The low edge of the first bin.
+        :param max_ell: The high edge of the last bin.
+        :return: The ell values.
         """
         minimum, midpoint, maximum, n_log = (
-            self.minimum,
+            self.minimum if min_ell is None else max(min_ell, self.minimum),
             self.midpoint,
-            self.maximum,
+            self.maximum if max_ell is None else max_ell,
             self.n_log,
         )
+        assert minimum < midpoint
+        assert midpoint < maximum
+
         lower_range = np.linspace(minimum, midpoint - 1, midpoint - minimum)
         upper_range = np.logspace(np.log10(midpoint), np.log10(maximum), n_log)
         concatenated = np.concatenate((lower_range, upper_range))
         # Round the results to the nearest integer values.
         # N.B. the dtype of the result is np.dtype[float64]
         return np.unique(np.around(concatenated)).astype(np.int64)
-
-
-def log_linear_ells(
-    *, minimum: int, midpoint: int, maximum: int, n_log: int
-) -> npt.NDArray[np.int64]:
-    """Create an array of ells to sample the power spectrum.
-
-    This is used for for real-space predictions. The result will contain
-    each integral value from min to mid. Starting from mid, and going up
-    to max, there will be n_log logarithmically spaced values.
-
-    All values are rounded to the nearest integer.
-
-    :param minimum: The low edge of the first bin.
-    :param midpoint: The high edge of the last in the linear range.
-    :param maximum: The high edge of the last bin.
-    :param n_log: The number of bins in the log section of the range.
-    """
-    return LogLinearElls(
-        minimum=minimum, midpoint=midpoint, maximum=maximum, n_log=n_log
-    ).generate()
 
 
 def generate_bin_centers(
@@ -106,24 +94,6 @@ def generate_bin_centers(
             return np.array((edges[1:] + edges[:-1]) / 2.0, dtype=np.float64)
         case _:
             raise ValueError(f"Unrecognized binning: {binning}")
-
-
-def calculate_ells_for_interpolation(
-    min_ell: int, max_ell: int
-) -> npt.NDArray[np.int64]:
-    """See log_linear_ells.
-
-    This method mixes together:
-        1. the default parameters in ELL_FOR_XI_DEFAULTS
-        2. the first and last values in w.
-
-    and then calls log_linear_ells with those arguments, returning whatever it
-    returns.
-    """
-    ell_config = copy.deepcopy(ELL_FOR_XI_DEFAULTS)
-    ell_config["maximum"] = max_ell
-    ell_config["minimum"] = max(ell_config["minimum"], min_ell)
-    return log_linear_ells(**ell_config)
 
 
 class EllOrThetaConfig(TypedDict):
@@ -168,57 +138,3 @@ def generate_reals(theta_config: EllOrThetaConfig):
     xis = np.zeros_like(thetas)
 
     return thetas, xis
-
-
-def apply_ells_min_max(
-    ells: npt.NDArray[np.int64],
-    Cells: npt.NDArray[np.float64],
-    indices: None | npt.NDArray[np.int64],
-    ell_min: None | int,
-    ell_max: None | int,
-) -> tuple[
-    npt.NDArray[np.int64], npt.NDArray[np.float64], None | npt.NDArray[np.int64]
-]:
-    """Apply the minimum and maximum ell values to the ells and Cells."""
-    if ell_min is not None:
-        locations = np.where(ells >= ell_min)
-        ells = ells[locations]
-        Cells = Cells[locations]
-        if indices is not None:
-            indices = indices[locations]
-
-    if ell_max is not None:
-        locations = np.where(ells <= ell_max)
-        ells = ells[locations]
-        Cells = Cells[locations]
-        if indices is not None:
-            indices = indices[locations]
-
-    return ells, Cells, indices
-
-
-def apply_theta_min_max(
-    thetas: npt.NDArray[np.float64],
-    xis: npt.NDArray[np.float64],
-    indices: None | npt.NDArray[np.int64],
-    theta_min: None | float,
-    theta_max: None | float,
-) -> tuple[
-    npt.NDArray[np.float64], npt.NDArray[np.float64], None | npt.NDArray[np.int64]
-]:
-    """Apply the minimum and maximum theta values to the thetas and xis."""
-    if theta_min is not None:
-        locations = np.where(thetas >= theta_min)
-        thetas = thetas[locations]
-        xis = xis[locations]
-        if indices is not None:
-            indices = indices[locations]
-
-    if theta_max is not None:
-        locations = np.where(thetas <= theta_max)
-        thetas = thetas[locations]
-        xis = xis[locations]
-        if indices is not None:
-            indices = indices[locations]
-
-    return thetas, xis, indices
