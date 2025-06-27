cf921e7da66d311e534fd44130282a4bd45f9029
Marc Paterno <paterno@fnal.gov>
2025-06-27 18:43:53 -0500

    Fix mypy complaint

--- firecrown/connector/numcosmo/numcosmo.py
+++ firecrown/connector/numcosmo/numcosmo.py
@@ -891,6 +891,7 @@ class NumCosmoGaussCov(Ncm.DataGaussCov):
                 calculator_args=self._nc_mapping.calculate_ccl_args(mset)
             )
         else:
+            assert self._nc_mapping is not None
             self._nc_mapping.p_ml.set_params_from_numcosmo(mset)
             params_map = create_params_map(self._model_list, mset, None)
             self.likelihood.update(params_map)
