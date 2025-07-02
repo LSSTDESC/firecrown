35b72db23e045a15664585853a005f1ac80158ff
eduardojsbarroso <eduardojsbarroso@gmail.com>
2025-07-02 14:26:44 +0200

    Refactored deltasigma and added systematics functions

--- firecrown/models/cluster/deltasigma.py
+++ firecrown/models/cluster/deltasigma.py
@@ -9,8 +9,9 @@ import numpy as np
 import numpy.typing as npt
 import pyccl
 from scipy.stats import gamma
+from scipy.integrate import simpson
 from firecrown.models.cluster.integrator.numcosmo_integrator import NumCosmoIntegrator
-
+import time
 import clmm  # pylint: disable=import-error
 from firecrown import parameters
 from firecrown.models.cluster.abundance import ClusterAbundance
@@ -46,8 +47,9 @@ class ClusterDeltaSigma(ClusterAbundance):
         radius_center: np.float64,
         two_halo_term: bool = False,
         miscentering_frac: np.float64 = None,
+        boost_factor: bool = False
     ) -> npt.NDArray[np.float64]:
-        """Delta sigma for clusters."""
+        """Delta sigma for cprint(new_pred)lusters."""
         cosmo_clmm = clmm.Cosmology()
         # pylint: disable=protected-access
         cosmo_clmm._init_from_cosmo(self._cosmo)
@@ -70,29 +72,39 @@ class ClusterDeltaSigma(ClusterAbundance):
             val = self._one_halo_contribution(moo, radius_center, redshift, miscentering_frac)
             if two_halo_term:
                 val+= self._two_halo_contribution(moo, radius_center, redshift)
+            if boost_factor:
+                val = self._correct_with_boost_nfw(val, radius_center)
             return_vals.append(val)
         return np.asarray(return_vals, dtype=np.float64)
 
 
-    def _one_halo_contribution(self, clmm_model: clmm.Modeling, radius_center, redshift, miscentering_frac = None) -> npt.NDArray[np.float64]:
+    def _one_halo_contribution(self, clmm_model: clmm.Modeling, radius_center, redshift, miscentering_frac = None, sigma_offset = 0.12) -> npt.NDArray[np.float64]:
         """Calculate the second halo contribution to the delta sigma."""
         # pylint: disable=protected-access
+        #t1 = time.time()
         first_halo_right_centered = clmm_model.eval_excess_surface_density(radius_center, redshift)
+        #t2 = time.time()
+        #print(f'1halo term took {t2-t1}')
         if miscentering_frac is not None:
-            integrator = NumCosmoIntegrator()
+            integrator = NumCosmoIntegrator(relative_tolerance = 1e-2, absolute_tolerance = 1e-6,)
             def integration_func(int_args, extra_args):
                 sigma = extra_args[0]
                 r_mis_list = int_args[:, 0]
+                #t3 = time.time()
                 esd_vals = np.array([
                     clmm_model.eval_excess_surface_density(np.array([radius_center]), redshift, r_mis=r_mis)[0]
                     for r_mis in r_mis_list
                 ])
+                #t4 = time.time()
+                #print(f'for {len(r_mis_list)} radiuses, misc it took {(t4 - t3) / len(r_mis_list)} per radius')
                 gamma_vals = gamma.pdf(r_mis_list, a=2.0, scale=sigma)
                 return esd_vals * gamma_vals
-            integrator.integral_bounds = [(0.0, 5.0)]
-            integrator.extra_args = np.array([0.12])  ## From https://arxiv.org/pdf/2502.08444
+            integrator.integral_bounds = [(0.0, 25.0 * sigma_offset)]
+            integrator.extra_args = np.array([sigma_offset])  ## From https://arxiv.org/pdf/2502.08444, we are using 0.12
+            #t5 = time.time()
             miscentering_integral = integrator.integrate(integration_func)
-            print(miscentering_integral)
+            #t6 = time.time()
+            #print(f'The miscentering integral took {t6 - t5}')
             return (1.0 - miscentering_frac) * first_halo_right_centered + miscentering_frac * miscentering_integral
         return first_halo_right_centered
 
@@ -112,3 +124,10 @@ class ClusterDeltaSigma(ClusterAbundance):
         )
         a = 1.0 / (1.0 + redshift)
         return conc_model._concentration(self._cosmo, 10.0**log_m, a)  # pylint: disable=protected-access
+    
+    def _correct_with_boost_nfw(self, profiles: npt.NDArray[np.float64], radius_list: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        """Determine the nfw boost factor and correct the shear profiles."""
+        boost_factors = clmm.utils.compute_powerlaw_boost(radius_list, 1.0)
+        #print(boost_factors, radius_list, profiles)
+        corrected_profiles = clmm.utils.correct_with_boost_values(profiles, boost_factors)
+        return corrected_profiles
