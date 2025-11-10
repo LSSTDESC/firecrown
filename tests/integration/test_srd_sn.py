d810fc76961480bf4fafb1ad28ddf7274245a99a
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-11-10 18:13:42 -0300

    Updating integration tests to use example generators.

--- tests/integration/test_srd_sn.py
+++ tests/integration/test_srd_sn.py
@@ -1,50 +1,71 @@
 """Integration tests for SRD SN"""
 
 import subprocess
+from pathlib import Path
 import pytest
 
+from firecrown.app.analysis import Frameworks
+from firecrown.app.examples import ExampleSupernovaSRD
 
-@pytest.mark.integration
-def test_srd_sn_cosmosis():
-    result = subprocess.run(
-        [
-            "bash",
-            "-c",
-            """
-                set -e
-                cd examples/srd_sn
-                cosmosis sn_srd.ini
-                cosmosis sn_only.ini
-            """,
-        ],
-        capture_output=True,
-        text=True,
-        check=True,
+
+@pytest.fixture(name="target_framework", params=Frameworks)
+def fixture_target_framework(request) -> Frameworks:
+    """Generate a target framework for all frameworks."""
+    return request.param
+
+
+@pytest.fixture(name="use_absolute_path", params=[True, False])
+def fixture_use_absolute_path(request) -> bool:
+    """Generate a use_absolute_path for all path types."""
+    return request.param
+
+
+@pytest.fixture(name="sn_srd_example")
+def fixture_sn_srd_example(
+    target_framework: Frameworks, use_absolute_path: bool, tmp_path: Path
+) -> tuple[Path, Frameworks]:
+    """Generate SN SRD example for all frameworks and path types."""
+    ExampleSupernovaSRD(
+        output_path=tmp_path,
+        prefix="sn_srd",
+        target_framework=target_framework,
+        use_absolute_path=use_absolute_path,
     )
 
-    assert result.returncode == 0
-    print(result.stdout)
+    return tmp_path, target_framework
 
 
 @pytest.mark.integration
-def test_srd_sn_numcosmo():
-    result = subprocess.run(
-        [
-            "bash",
-            "-c",
-            """
-                set -e
-                cd examples/srd_sn
-                numcosmo from-cosmosis sn_srd.ini
-                numcosmo run test sn_srd.yaml
-                numcosmo from-cosmosis sn_only.ini
-                numcosmo run test sn_only.yaml
-            """,
-        ],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
+def test_srd_sn_run(sn_srd_example):
+    output_path, framework = sn_srd_example
+    match framework:
+        case Frameworks.COSMOSIS:
+            result = subprocess.run(
+                ["cosmosis", "cosmosis_sn_srd.ini"],
+                cwd=output_path,
+                capture_output=True,
+                text=True,
+                check=True,
+            )
 
-    assert result.returncode == 0
-    print(result.stdout)
+            assert result.returncode == 0
+        case Frameworks.COBAYA:
+            result = subprocess.run(
+                ["cobaya-run", "-f", "cobaya_sn_srd.yaml"],
+                cwd=output_path,
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+
+            assert result.returncode == 0
+        case Frameworks.NUMCOSMO:
+            result = subprocess.run(
+                ["numcosmo", "run", "test", "numcosmo_sn_srd.yaml"],
+                cwd=output_path,
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+
+            assert result.returncode == 0
