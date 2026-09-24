4464cea92ff7fe36eae3f164774191346be8f153
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2026-09-24 16:55:21 -0300

    Exporting all external API factories.

--- firecrown/likelihood/number_counts/__init__.py
+++ firecrown/likelihood/number_counts/__init__.py
@@ -1,21 +1,45 @@
 """Number counts source and systematics."""
 
 from firecrown.likelihood.number_counts._factories import (
+    ConstantMagnificationBiasSystematicFactory,
+    LinearBiasSystematicFactory,
+    MagnificationBiasSystematicFactory,
     NumberCountsFactory,
+    NumberCountsSystematicFactory,
+    PTNonLinearBiasSystematicFactory,
 )
 from firecrown.likelihood.number_counts._source import NumberCounts
 from firecrown.likelihood.number_counts._systematics import (
     ConstantMagnificationBiasSystematic,
+    LinearBiasSystematic,
     MagnificationBiasSystematic,
     PhotoZShift,
+    PhotoZShiftandStretch,
     PTNonLinearBiasSystematic,
+    SelectField,
+)
+
+# Re-export shared factories from base module
+from firecrown.likelihood_base import (
+    PhotoZShiftandStretchFactory,
+    PhotoZShiftFactory,
 )
 
 __all__ = [
     "NumberCounts",
     "NumberCountsFactory",
+    "NumberCountsSystematicFactory",
     "PhotoZShift",
+    "PhotoZShiftFactory",
+    "PhotoZShiftandStretch",
+    "PhotoZShiftandStretchFactory",
+    "SelectField",
     "ConstantMagnificationBiasSystematic",
+    "ConstantMagnificationBiasSystematicFactory",
+    "LinearBiasSystematic",
+    "LinearBiasSystematicFactory",
     "PTNonLinearBiasSystematic",
+    "PTNonLinearBiasSystematicFactory",
     "MagnificationBiasSystematic",
+    "MagnificationBiasSystematicFactory",
 ]
